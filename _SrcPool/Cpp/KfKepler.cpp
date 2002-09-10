1b7d600dc41c545cde45a9d85eb750397f8b2323
ddarko <ddarko>
2002-09-10 15:01:28 +0000

    Added

---
+++ _SrcPool/Cpp/KfKepler.cpp
@@ -0,0 +1,52 @@
+/*$Workfile: KfKepler.cpp$: implementation file
+  $Revision: 1$ $Date: 9/5/02 9:22:28 PM$
+  $Author: Darko$
+
+  Solves a quadratic equation
+  CommonSoft Inc.
+  Mar 2k1. D.Kolakovic
+*/
+
+/* Group=Mathematics                                                         */
+
+/*Note: MS VC/C++ - Disable precompiled headers (/Yu"StdAfx.h" option)       */
+
+#include  "KProgCst.inl"  //IsZero()
+
+//funcKepler()-----------------------------------------------------------------
+/*Solves Kepler's equation of a body orbiting on an ellipse:
+
+    M = E -e*sin(E);
+    where M is mean anomaly, E is eccentric anomaly and e is eccentricity of 
+    an ellipse.
+
+  The ellipse eccentricity is the fraction of the distance to the semimajor axis
+  at which the focus lies and has range 0 < e < 1.
+
+  Returns: eccentric anomaly within rounding error [rad]  
+ */
+double funcKeplerEq(double dMeanAnomaly, //[in] mean anomaly [rad]
+                    double dEllipseEccentricity //[in] eccentricity of a body orbiting
+                    //on an ellipse [0, 1]
+                  )
+{
+double dEccentricAnomaly; 
+double dError;
+
+dEccentricAnomaly = dMeanAnomaly; //initialize [rad]
+do
+  {
+  // E - e*sin(E) - M = 0;
+  dError = dEccentricAnomaly - 
+           dEllipseEccentricity * sin(dEccentricAnomaly) - dMeanAnomaly;
+  dEccentricAnomaly -= dError / (1 - dEllipseEccentricity * cos(dEccentricAnomaly));
+  } while (!IsZero(dError));
+return dEccentricAnomaly;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ * $Log: 
+ *  1    Biblioteka1.0         9/5/02 9:22:28 PM    Darko           
+ * $
+ *****************************************************************************/
