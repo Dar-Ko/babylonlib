b1e3b959dfbf5d77c7df28e23119aa62f6d60fe6
ddarko <ddarko>
2006-11-07 23:05:48 +0000

    Added WSH test case

--- _SrcPool/Js/KHasHSpace.js
+++ _SrcPool/Js/KHasHSpace.js
@@ -1,10 +1,75 @@
 //$Workfile: KHasHSpace.js$: script file
-//$Revision: 2$ $Date: 2006-11-01 13:39:42$
+//$Revision: 3$ $Date: 2006-11-07 18:04:13$
 //$Author: Darko Kolakovic$
 //
 //Verfy a string for white spaces
 //Copyright: CommonSoft Inc
 //2006-10 Darko Kolakovic
+
+//eval(Include("E:\\MyDocuments\\BabelLib\\_SrcPool\\Js\\KValidate.js"));
+
+test();
+function test()
+{
+try
+  {
+  var strValue = new Array();
+  strValue[0] = "abcdesdfsdh";
+  strValue[1] = "abcd esdfsdh";
+  strValue[2] = "abcde\tsdfsdh";
+  strValue[3] = "abcd\t.esdf sdh";
+  strValue[4] = "   abcdesdfsdh";
+  strValue[5] = "abcdesdfsdh\t";
+  strValue[6] = "abcde\asdfsdh";
+  var cachedFilespecs;
+  var i = 0;
+  while (i < strValue.length)
+    {
+    var bRes = hasHSpace(strValue[i++]);
+    WScript.echo((i-1) + ". " +strValue[i-1] + " match " + bRes);
+    }
+  }
+catch(error)
+  {
+  WScript.echo(error.name + ": " + error.message +
+               "; text " + i +  ". " + strValue[i]);
+  }
+}
+
+//------------------------------------------------------------------------------
+/*Verifies object existence.
+
+  Returns: true if object not exist or if object has no value.
+  Otherwise returns false.
+ */
+function isBlank(oValue //[in] object to validate
+                )
+{
+return ((oValue == null) || (oValue.length == 0));
+}
+
+//-----------------------------------------------------------------------------
+/*Note: Microsoft Windows specific (Win32)
+ */
+function Include(szFileName //[in] name of the file to import
+                )
+{
+if( szFileName == undefined || szFileName == "")
+  return;
+try
+  {
+  fso = new ActiveXObject("Scripting.FileSystemObject");
+  fileImport = fso.OpenTextFile(szFileName);
+  strImport = fileImport.ReadAll();
+  fileImport.Close();
+  return strImport;
+  }
+catch(error)
+  {
+  WScript.echo(error.name + ": " + error.message +
+               "; file '" + szFileName +  "'.");
+  }
+}
 
 //------------------------------------------------------------------------------
 /*Verifies if a string has white spaces.
@@ -17,9 +82,9 @@ function hasHSpace(strValue //[in] string to be evaluated
 {
 if(!isBlank(strValue))
   {
-  for(var i=0; i<strValue.length; i++)
+  for(var i = 0; i < strValue.length; i++)
     {
-    if ((strValue.charAt(i)==" ") || (strValue.charAt(i)=="\t"))
+    if ((strValue.charAt(i) == ' ') || (strValue.charAt(i) == '\t'))
       {
       return true;
       }
