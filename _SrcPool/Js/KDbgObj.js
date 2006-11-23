a46ebd03bd970fb09324d2ae8dbe7d49372decc4
ddarko <ddarko>
2006-11-23 23:21:35 +0000

    Fixed function dump

--- _SrcPool/Js/KDbgObj.js
+++ _SrcPool/Js/KDbgObj.js
@@ -1,5 +1,5 @@
 //$Workfile: KDbgObj.js$: script file
-//$Revision: 4$ $Date: 2006-11-22 19:09:00$
+//$Revision: 6$ $Date: 2006-11-23 18:20:21$
 //$Author: Darko Kolakovic$
 //
 //Dumps variable type and value
@@ -17,7 +17,7 @@ function dbgObj(anyValue //[in] variable of any kind
                )
 {
 var strResult;
-if((anyValue == null))// || (anyValue == 'undefined'))
+if((anyValue == null))
   strResult = "undefined: null";
 else if((anyValue == undefined))
   strResult = "undefined: null";
@@ -29,7 +29,8 @@ else
     strResult = strType + ": ";
     //The callee property is a member of the arguments object that becomes
     //available only when the associated function is executing.
-    if((anyValue.arguments != undefined) && (anyValue.arguments.callee != undefined))
+    if( ((anyValue.arguments != undefined) && (anyValue.arguments.callee != undefined)) ||
+        ((anyValue.constructor != undefined) && (anyValue.constructor == Function)) )
       {
       //Get function's name
       var regexMatch = anyValue.toString().match(/function(\s*)(\w*)/);
@@ -39,21 +40,28 @@ else
         strResult += 'anonymous(';
 
       //Dump function arguments
-      var i = 0; //Argument 0 is caller function
-      while(i < anyValue.arguments.length) //Get arguments passed to function.
+      if(anyValue.arguments != undefined)
         {
-        if(typeof(anyValue.arguments[i]) == "string") //Quote a string argument
-          strResult += "'" + anyValue.arguments[i];
-        else if ((typeof(anyValue.arguments[i]) == "function") ||  
-                 (typeof(anyValue.arguments[i]) == "object"))
-          strResult += typeof(anyValue.arguments[i]);
-        else
-          strResult += anyValue.arguments[i];
-        strResult += ",";
-        i++;
+        var i = 0; //Argument 0 is caller function
+        while(i < anyValue.arguments.length) //Get arguments passed to function.
+          {
+          if(typeof(anyValue.arguments[i]) == "string") //Quote a string argument
+            strResult += "'" + anyValue.arguments[i];
+          else if ((typeof(anyValue.arguments[i]) == "function") ||
+                   (typeof(anyValue.arguments[i]) == "object"))
+            strResult += typeof(anyValue.arguments[i]);
+          else
+            strResult += anyValue.arguments[i];
+          strResult += ",";
+          i++;
+          }
+        if(strResult.charAt(strResult.length-1) == ',')
+          strResult = strResult.substr(0, strResult.length-1);
         }
-      if(strResult.charAt(strResult.length-1) == ',')
-        strResult = strResult.substr(0, strResult.length-1);
+      else
+        {
+        strResult += anyValue.length + " param"; //Number of expected arguments
+        }
       strResult += ")";
       }
     else
@@ -63,20 +71,41 @@ else
     }
   else
     {
+    var strElements = "";
     //Validate object for two common Array methods
     if((anyValue.concat != undefined) && (anyValue.sort != undefined))
-      strType = "Array";    //The object appears to be an array
-    strResult = strType + ": ";
-    var strElements = "";
-    for (var j in anyValue) //Dump array items or enumerable object properties
       {
-      //The undefined elements would be skiped over
-      strElements += "[" + j + "] " + anyValue[j] + "\n";
+      strType = "Array: ";    //The object appears to be an array
+      for (var j in anyValue) //Dump array items
+        {
+        //The undefined elements would be skiped over
+        strElements += "[" + j + "] " + anyValue[j] + "\n";
+        if (strElements.length > 256) //Limit the size of the output
+          {
+          strElements += "...";
+          break;
+          }
+        }
+      if(strElements == "")
+        strElements =  "{empty}";
       }
-    if(strElements == "")
-      strResult += (strType == "object") ? "(...)" : "{empty}";
-    else
-      strResult += strElements;
+    else //Other objects
+      {
+      strType += ": ";
+      for (var j in anyValue) //Dump enumerable object properties
+        {
+        //The undefined elements would be skiped over
+        strElements += "[" + j + "]\n"; //Limit the size of the output
+        if (strElements.length > 256)
+          {
+          strElements += "...";
+          break;
+          }
+        }
+      if(strElements == "")
+        strElements = "(...)";
+      }
+    strResult = strType + strElements;
     }
   }
 return strResult;
