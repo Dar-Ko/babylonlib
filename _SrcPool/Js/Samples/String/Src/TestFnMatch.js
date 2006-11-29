98432f72a0e4df5b3a5984a45db8c1c80f59fe83
ddarko <ddarko>
2006-11-29 16:51:21 +0000

    Workaround IE 6.0 bug that generates error #1019

--- _SrcPool/Js/Samples/String/Src/TestFnMatch.js
+++ _SrcPool/Js/Samples/String/Src/TestFnMatch.js
@@ -1,5 +1,5 @@
 /*$Workfile: TestFnMatch.js$: script file
-  $Revision: 2$ $Date: 2006-11-27 17:43:48$
+  $Revision: 4$ $Date: 2006-11-29 11:50:04$
   $Author: Darko Kolakovic$
 
   Test filename pattern matching
@@ -17,44 +17,86 @@
 function TestFnMatch()
 {
 TsWriteToViewLn("TestFnMatch()");
-var bRes = true;
+var bRes = true;   //result of the test
+var bMatch = true; //result of the pattern matching
 //TESTENTRY logEntry =
-//  {_T("FnMatch()"), _T("KFnMatch.js"), bRes};
-debugger;//
+//  {_T("FnMatch()"), _T("KFnMatch.js"), bMatch};
+//debugger;
+var i = 0;
 try
   {
-  var szFilename = "Dario [Augustus].ma"
-  var szPattern = new Array();
-  szPattern[ 0] = "D* ].?a";
-  szPattern[ 1] = "[xyz]";
-  szPattern[ 2] = "[\\x\\y^z]";
-  szPattern[ 3] = "[!xyz]";
-  szPattern[ 4] = "[x^^yz]";
-  szPattern[ 5] = "[xyz"; //not terminated set
-  szPattern[ 6] = "[!!xyz]";
-  szPattern[ 7] = "[\\x\\y^z]";
-  szPattern[ 8] = "[ x\tyz]";
-  szPattern[ 9] = "![xyz]";
-  szPattern[10] = "[]"; //empty set
-  szPattern[11] = "[]]";
-  szPattern[12] = "[[]";
-  szPattern[13] = "[\\]]";
-  szPattern[14] = "D[!xyz]*.ma";
-
-  for (var i in szPattern)
+  /*Container class for testing filename pattern matching.
+    The special characters used in shell-style wildcards are:
+      *       matches any number of characters
+      ?       matches any single character
+      [set]   matches any character in the set
+      [!set]  matches any character not in set
+   */
+  function CTestCase (strPattern, //='*' [in] Unix filename pattern
+                     bTestResult //=false [in] expected result of the test
+                     )
     {
-    var bRes = FnMatch(szFilename, szPattern[i], true);
-    TsWriteToViewLn("  " + szPattern[i] + " match " + bRes);
+    this.strPattern = "*";
+    this.bResult = false;
+    if (strPattern != undefined)
+      this.strPattern = "" + strPattern; //string by contract
+    if (bTestResult != undefined)
+      this.bResult = (bTestResult == true); //boolean by contract
+    };
+
+  var szFilename = "Dario [Augustus].ma"
+  var arrPattern = new Array();
+  arrPattern[ 0] = new CTestCase("D*].?a",      true );
+  arrPattern[ 1] = new CTestCase("[xyz]",       false);
+  arrPattern[ 2] = new CTestCase("[\\x\\y^z]",  false);
+  arrPattern[ 3] = new CTestCase("[!xyz]",      true );
+  arrPattern[ 4] = new CTestCase("[x^^yz]",     false);
+  arrPattern[ 5] = new CTestCase("[xyz",        false); //not terminated set
+  arrPattern[ 6] = new CTestCase("[!!xyz]",     true );
+  arrPattern[ 7] = new CTestCase("[\\x\\y^z]",  false);
+  arrPattern[ 8] = new CTestCase("[ x\tyz]",    false);
+  arrPattern[ 9] = new CTestCase("![xyz]",      false);
+  arrPattern[10] = new CTestCase("[]",          false); //empty set
+  arrPattern[11] = new CTestCase("[]]",         false);
+  arrPattern[12] = new CTestCase("[[]",         false);
+  arrPattern[13] = new CTestCase("[\\]]",       false);
+  arrPattern[14] = new CTestCase("D[!xyz]*.ma", true );
+
+  TsWriteToViewLn(szFilename);
+  var arrCache = new Array(); //regular expression cache
+  while(i < arrPattern.length)
+    {
+    bMatch = FnMatch(szFilename, arrPattern[i].strPattern, true, arrCache);
+    TsWriteToViewLn("" + i + ". " + arrPattern[i].strPattern + " match " + bMatch);
+    if (bMatch != arrPattern[i].bResult)
+      {
+      bRes = false;
+      break; 
+      /*Bug: IE 6.0 generates error #1019 "Error: Can't have 'break' outside of loop"
+        The error is caused by the character > 0x7F inside the comment line: see the word
+        cache in the comment above (used to be cache')!
+       */
+      }
+    i++;
+    }
+  if (bRes)
+    {
+    //Test a pattern from the RegExp cache
+    //Note: in the case of an error, the message will show index i. See catch().
+    i++;
+    bMatch = FnMatch(szFilename, arrPattern[0].strPattern, true, arrCache);
+    TsWriteToViewLn(i + ". " + arrPattern[0].strPattern + " match " + bMatch);
+    bRes = (bMatch == arrPattern[0].bResult);
     }
   }
 catch(error)
   {
   TsWriteToViewLn(error.name + ": " + error.message +
-               "; pattern " + i +  ". " + szPattern[i]);
-  bRes = false
+               "; pattern " + i +  ". " + arrPattern[i].strPattern);
+  bRes = false;
   }
 
-//logEntry.m_bResult = bRes;
+//logEntry.m_bResult = bMatch;
 //LogTest(&logEntry);
 
 TsWriteToViewLn(LOG_EOT);
