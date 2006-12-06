83175811ed69e2184faed11b295e3da1f3bd9e6b
ddarko <ddarko>
2006-12-06 22:58:21 +0000

    CLogger.Create()

--- _SrcPool/Js/KLogger.js
+++ _SrcPool/Js/KLogger.js
@@ -1,126 +1,67 @@
 //$Workfile: KLogger.js$: script file
-//$Revision: 3$ $Date: 2006-12-04 17:52:17$
+//$Revision: 5$ $Date: 2006-12-06 17:57:20$
 //$Author: Darko Kolakovic$
 //
 //Events or messages recorder
 //Copyright: CommonSoft Inc
 //2006-12 Darko Kolakovic
 
+///////////////////////////////////////////////////////////////////////////////
+// Constants
+var LOG_DOM    = 0; //logbook is DOM element with id=ID_LOG
+var LOG_WND    = 1; //logbook is pop-up window with name logbook
+var LOG_FILE   = 2; //logbook is file with default name event.log
+var LOG_STDOUT = 3; //logging to standard output or to the current HTML
+                    //document, depending of script shell.
+
+///////////////////////////////////////////////////////////////////////////////
 //-----------------------------------------------------------------------------
 /*A class that records events or messages to an output stream or a log file.
 
  */
-function CLogger()
+function CLogger(iOutputType //=LOG_DOM [in] log type
+                )
 {
+
 this.tmStart= new Date().getTime(); //time of log creation in ms since
                                     //1970-01-01T00:00:00
 this.m_hLogOut = null;              //output stream
-this.m_hLogOut = new CLogWnd();
-//this.getWndOut(); //init();
-               //this.getDivOut();
+this.m_iType = (iOutputType == null)  ?
+               LOG_DOM : (+iOutputType); //plus sign converts var to number
+this.m_hLogOut = this.Create(this.m_iType);
 };
 
 //-----------------------------------------------------------------------------
-/*Creates the new window, named LOGBOOK, as destination for log entries.
-  The ID of the area for the logs is ID_LOGBOOK.
+/*Creates logger target object.
 
   Returns the Logbook object if successful; otherwise returns null;
  */
-CLogger.prototype.getWndOut = function()
-{
-hWnd = window.open("", //Optional URL of the document or blank page.
-                       "LOGBOOK", //Optional target Id to be used in forms
-                       ("left=10,"   + //left position relative to
-                                       //the upper-left corner of the screen [px]
-                       "top=20,"     + //top position relative to
-                                       //the upper-left corner of the screen [px]
-                       "width=600,"  + //width of the window [px]
-                       "height=450," + //height of the window [px]
-                       "scrollbars=yes," + //display horizontal and vertical scroll bars
-                       "resizable=yes," + //display resize handles
-                       "status=no," +     //display a status bar
-                       "toolbar=no," +    //display browser's toolbar
-                       "location=no"      //display the input field for URLs
-                      ));
-if (hWnd == null)
-  return null; //Failed to create new pop-up window;
-               //probably pop-up blocking is enabled.
-
-  //Create empty logbook document
-if (hWnd.document != null)
-  {
-  hWnd.document.open();
-  hWnd.document.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" ' +
-                      '  "http://www.w3.org/TR/html4/loose.dtd">' +
-                      "<html>\n<head>\n" +
-                      "<title>Logbook<\/title>\n" +
-                      "<style type='text/css'>\n" +
-                      "  .DEBUG{ color: green; }\n" +
-                      "  .INFO{ color: navy; }\n" +
-                      "  .WARN{ color: black; }\n" +
-                      "  .ERROR{ color: red; }\n" +
-                      "<\/style>\n" +
-                      "<\/head>\n" +
-                      "<body>\n<div id='ID_LOG_JOK'>\n<\/div>\n<\/body>\n" +
-                      "<\/html>\n");
-  hWnd.document.close();
-  }
-
-if (hWnd.document.getElementById == undefined)
-  return null; //IE 5.0 missing method.
-
-var hLog = hWnd.document.getElementById('ID_LOG');
-if (hLog == null) //Create missing element
-  return this.getDivOut(hWnd.document);
-return hLog;
-};
-
-//-----------------------------------------------------------------------------
-/*Appends the new text area with Id ID_LOG to the existing document, to be
-  the destination for the log entries.
-
-  Returns the Logbook object if successful; otherwise returns null;
- */
-CLogger.prototype.getDivOut = function(oDocument //=null [in] the document where
+CLogger.prototype.Create = function(iType //=null [in] the document where
                                        //log entries will appear or, if null,
                                        //the current document
                                       )
 {
-if (oDocument == null)
-  oDocument = document;
-if (oDocument == null)
-  return null; //Failed to obtain the working document
-if (oDocument.getElementById == undefined)
-  return null; //IE 5.0 missing method.
-
-var hBody = oDocument.getElementsByTagName("body");
-if (hBody == null)
-  return null;
-
-var hLog = oDocument.getElementById('ID_LOG');
-if (hLog == null) //Create missing element
+var hLog = null;
+switch (iType)
   {
-  var hLogbook = oDocument.createElement("div"); //Wrapper element
-  hLogbook.setAttribute("id","ID_LOGBOOK");
-  hBody.item(0).appendChild(hLogbook);
-  hLogbook.appendChild(oDocument.createElement("hr"));
-  hLog = oDocument.createElement("div");
-  hLogbook.setAttribute("id","ID_LOG");
-  hLogbook.appendChild(hLog);
-  hLogbook.appendChild(oDocument.createElement("hr"));
+  case LOG_DOM:  //Output to DOM element with id=ID_LOG
+    if((hLog = new CLogElement()) != null)
+      break;
+    //Try next target
+  case LOG_WND:  //Output to pop-up window
+    if((hLog = new CLogWnd()) != null)
+      break;
+    //Try next target
+  case LOG_FILE: //Output to a file, if file system is avaliable
+    if((hLog = new CLogWnd()) != null) //TODO
+      break;
+    //Try next target
+  case LOG_STDOUT: //Output to the standard stream or to the current HTML document
+  default:
+    hLog = new CLogWnd(); //TODO
   }
-hLogbook.setAttribute("style","left: 16px; width: 90%; position: relative;");
-
-//If ownerDocument property is not supported, add new one
-if(hLogbook.ownerDocument == null)
-  hLogbook.ownerDocument = oDocument;
-if(hLog.ownerDocument == null)
-  hLog.ownerDocument = oDocument;
-
-hLog.m_hOwner = this; //owner object of the logbook
-
 return hLog;
-};
+}
 
 //-----------------------------------------------------------------------------
 /*Writes new entry to the logbook, if the log is created and the text is not
@@ -131,14 +72,13 @@ return hLog;
 CLogger.prototype.Append = function(strEntry //[in] text to be added to the log
                                     )
 {
-if ((strEntry != null) && (strEntry != ""))
+if ((this.m_hLogOut != null) && (this.m_hLogOut.Append != null))
   {
-  if ((this.m_hLogOut != null) && (this.m_hLogOut.Append != null))
-    {
-    return this.m_hLogOut.Append(strEntry,
-                                (new Date().getTime() - this.tmStart) + "ms");
-    }
+  return this.m_hLogOut.Append(strEntry,
+                              (new Date().getTime() - this.tmStart) + "ms");
   }
+if(document != null) //Try to salvage the message
+  document.writeln(strEntry);
 return false;
 };
 
@@ -178,13 +118,12 @@ if (oDocument == null)
 if (oDocument.getElementById == undefined)
   return null; //IE 5.0 missing method.
 
-var hBody = oDocument.getElementsByTagName("body");
-if (hBody == null)
-  return null;
-
 var hLog = oDocument.getElementById('ID_LOG');
 if (hLog == null) //Create missing element
   {
+  var hBody = oDocument.getElementsByTagName("body");
+  if (hBody == null)
+    return null;
   var hLogbook = oDocument.createElement("div"); //Wrapper element
   hLogbook.setAttribute("id","ID_LOGBOOK");
   hBody.item(0).appendChild(hLogbook);
@@ -204,7 +143,8 @@ return hLog;
 }
 
 //-----------------------------------------------------------------------------
-/*Appends the new log entry to the logbook text area.
+/*Appends the new log entry to the logbook text area. The empty entry will be
+  replaced with non-breakable space.
 
   Returns true if successful; otherwise returns false;
  */
@@ -214,28 +154,27 @@ CLogElement.prototype.Append = function(strEntry, //[in] text to be added to the
                                         //time in ms as default entry title
                                        )
 {
-if ((strEntry != null) && (strEntry != ""))
+if (this.ownerDocument != null)
   {
-  if (this.ownerDocument != null)
-    {
-    //Note: createElement(any) requires Internet Explorer 5.
-    var eEntry = this.ownerDocument.createElement("span");
-    eEntry.className = "logentry";
-    //Add line number
-    eEntry.title = ((strEntryTag != null) && (strEntryTag != "")) ?
-                   strEntryTag : (new Date().getTime() + "ms");
-    eEntry.appendChild(this.ownerDocument.createTextNode(strEntry));
-    this.appendChild(eEntry);
-    //Add new line
-    this.appendChild(this.ownerDocument.createElement("br"));
-    //Make the last entry visible in the viewport
-    if (this.offsetHeight > this.scrollHeight)
-      this.scrollTop = 0;
-    else
-      this.scrollTop = this.scrollHeight;
+  if ((strEntry == null) || (strEntry == ""))
+    strEntry = "&nbsp;" //FixMe! string seen is &amp;nbsp; - not ' ' as supposed to be! D.K.
+  //Note: createElement(any) requires Internet Explorer 5.
+  var eEntry = this.ownerDocument.createElement("span");
+  eEntry.className = "logentry";
+  //Add line number
+  eEntry.title = ((strEntryTag != null) && (strEntryTag != "")) ?
+                 strEntryTag : (new Date().getTime() + "ms");
+  eEntry.appendChild(this.ownerDocument.createTextNode(strEntry));
+  this.appendChild(eEntry);
+  //Add new line
+  this.appendChild(this.ownerDocument.createElement("br"));
+  //Make the last entry visible in the viewport
+  if (this.offsetHeight > this.scrollHeight)
+    this.scrollTop = 0;
+  else
+    this.scrollTop = this.scrollHeight;
 
-    return true;
-    }
+  return true;
   }
 return false;
 }
@@ -303,65 +242,6 @@ if (hWnd.document != null)
 //Return log element
 var hLog = new CLogElement(hWnd.document);
 return hLog;
-};
-
-////////////////////////////////////////////////////////////////////////////////
-//-----------------------------------------------------------------------------
-/*A log record of events. Stores time of the event, description and
-  the event grade.
-*/
-function CLogEntry(strMessage, //="" [in] event description
-                   iLevel      //=null [in] event rank [NONE, ERROR]
-                  )
-{
-this.m_dateRecord = new Date();                    //log entry time
-this.m_strRecord = (strMessage ? strMessage : ""); //log record
-this.m_iLevel = (iLevel? iLevel : 0);              //log record rank
-
-if (this.m_dateRecord.getFullYear == undefined)
-  {
-  /*Returns year in the Gregorian Calendar, represented by four digits in
-    the range [0000, 9999]. (JavaScript 1.0)
-   */
-  this.m_dateRecord.getFullYear = function()
-    {
-    var iYear = this.getYear();
-    if(iYear < 100)
-      iYear += 1900;
-    return iYear;
-    }
-  };
-
-/*Returns date and time in extended format described by ISO 8601:2000
-  'Data Elements and Interchange Formats' standard.
- */
-this.m_dateRecord.toISOString = function()
-  {
-  var iYear = this.getFullYear();
-  var vMonth = this.getMonth() + 1;
-  vMonth = (vMonth < 10 ? "0" : "") + vMonth;
-  var vDay = this.getDate();
-  vDay = (vDay < 10 ? "0" : "") + vDay;
-  var vHour = this.getHours();
-  vHour = (vHour < 10 ? "0" : "") + vHour;
-  var vMin = this.getMinutes();
-  vMin = (vMin < 10 ? "0" : "") + vMin;
-  var vSec = this.getSeconds();
-  vSec = (vSec < 10 ? "0" : "") + vSec;
-
-  return (iYear + "-" + vMonth  + "-" + vDay + "T" +
-          vHour + ":" + vMin    + ":" + vSec           );
-  };
-};
-
-/*Returns formatted log entry:
-      YYYY-MM-DDThh:mm:ss:\tLevel:\tdescription
- */
-CLogEntry.prototype.toString = function()
-{
-return this.m_dateRecord.toISOString() + ":\t" +
-       this.m_iLevel + ":\t" +
-       this.m_strRecord;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
--- _SrcPool/Js/Samples/Log/Src/TestLogger.js
+++ _SrcPool/Js/Samples/Log/Src/TestLogger.js
@@ -1,5 +1,5 @@
 /*$Workfile: TestLogger.js$: script file
-  $Revision: 4$ $Date: 2006-12-05 19:17:29$
+  $Revision: 5$ $Date: 2006-12-06 17:57:24$
   $Author: Darko Kolakovic$
 
   Test Logger
@@ -24,16 +24,12 @@ try
   {
   /*Container class for testing string inquisition.
    */
-  function CTestCase (oValue, //[in] object to be evaluated
-                      strValue, //="" [in] expected test object string representation
+  function CTestCase (strValue, //="" [in] log entry
                       bTestResult //=false [in] expected result of the test
                      )
     {
-    this.oValue = "<null>";
     this.strValue = "";
     this.bResult = false;
-    if (oValue != undefined)
-      this.oValue = oValue; //object
     if (strValue != undefined)
       this.strValue += strValue; //string by contract
     if (bTestResult != undefined)
@@ -41,11 +37,17 @@ try
     };
 
   TsWriteToViewLn("Test CLogger");
-  TsWriteToViewLn("Test CLogWnd");
   TsWriteToViewLn("Test CLogElement");
 
   //TESTENTRY logEntry =
   //  {_T("CLogger()"), _T("KLogger.js"), bRes};
+  var eLog = document.getElementById('ID_LOG'); //Check for static logbook
+  if (eLog == null)
+    {
+    TsWriteToViewLn("ID_LOG = " + eLog);
+    TsWriteToViewLn("Is the element after the script invocation?");
+    }
+
   var oLog = new CLogger(); //logbook
   bRes = (oLog != undefined);
   if (bRes && (oLog.tmStart != undefined))
@@ -59,79 +61,66 @@ try
     //  {_T("CLogger::Append()"), _T("KLogger.js"), bRes};
   if (bRes && (oLog.Append != undefined))
     {
-    TsWriteToViewLn("New log entry in Logbook window.");
-    bRes = (oLog.Append("Ursus arctos horribilis"));
+    TsWriteToViewLn("New log entry in the Logbook.");
+    bRes = (oLog.Append("Macropus giganteus"));
     }
   else
     {
-    TsWriteToViewLn("Are pop-up windows blocked?");
     bRes = false;
     }
     //logEntry.m_bResult = bRes;
     //LogTest(&logEntry);
 
-  //logEntry.m_bResult = bRes;
-  //LogTest(&logEntry);
-  if(bRes)
-    {
-    TsWriteToViewLn("Test CLogEntry");
-    //TESTENTRY logEntry =
-    //  {_T("CLogEntry()"), _T("KLogger.js"), bRes};
-    var oLogEntry = new CLogEntry(); //empty log record
-    bRes = (oLogEntry != undefined);
-    if (bRes && (oLogEntry.m_dateRecord != undefined))
-      {
-      TsWriteToViewLn("Log entry created at " + oLogEntry.m_dateRecord.toUTCString() + " UTC");
-      if (oLogEntry.m_dateRecord.toISOString != undefined )
-        {
-        TsWriteToViewLn("Log entry created at " + oLogEntry.m_dateRecord.toISOString());
-        }
-      else
-        bRes = false;
-      }
-    else
-      bRes = false;
-    //logEntry.m_bResult = bRes;
-    //LogTest(&logEntry);
-    }
-
-    //TESTENTRY logEntry =
-    //  {_T("CLogEntry::toString()"), _T("KLogger.js"), bRes};
-  if (bRes && (oLogEntry.toString != undefined))
-    {
-    TsWriteToViewLn("Log empty entry is: " + oLogEntry.toString());
-    }
-  else
-    bRes = false;
-    //logEntry.m_bResult = bRes;
-    //LogTest(&logEntry);
-
-
+  var arrValue = new Array();
   if (bRes)
     {
-    var arrValue = new Array();
-    arrValue[ 0] = new CTestCase(new CLogEntry("Ursus arctos",       0));
-    arrValue[ 1] = new CTestCase(new CLogEntry("Ursus thibetanus",      1));
-    arrValue[ 2] = new CTestCase(new CLogEntry("Ursus maritimus",     2));
-    arrValue[ 3] = new CTestCase(new CLogEntry("Ursus thibetanus",   3));
-    arrValue[ 4] = new CTestCase(new CLogEntry("Ursus americanus",    4));
-    arrValue[ 5] = new CTestCase(new CLogEntry("Ursus spelaeus",     5));
-    arrValue[ 6] = new CTestCase(new CLogEntry("Ursus malayanus",     6));
-    arrValue[ 7] = new CTestCase(new CLogEntry("",                  7));
-
+    arrValue[ 0] = new CTestCase("Macropus agilis"     , true);
+    arrValue[ 1] = new CTestCase("Macropus fuliginosus", true);
+    arrValue[ 2] = new CTestCase("Macropus rufus"      , true);
+    arrValue[ 3] = new CTestCase("Macropus robustus"   , true);
+    arrValue[ 4] = new CTestCase("Macropus antilopinus", true);
+    arrValue[ 5] = new CTestCase("Macropus rufogriseus", true);
+    arrValue[ 6] = new CTestCase("Macropus irma"       , true);
+    arrValue[ 7] = new CTestCase(""                    , true);
+    TsWriteToViewLn("Append new records:");
     while(i < arrValue.length)
       {
       var bLogEntry = true; //result of the validation
-      TsWriteToViewLn("" + i + ". " + arrValue[i].oValue);
-      oLog.Append(arrValue[i].oValue);
-      //if (bHasSpace != arrValue[i].bResult)
-      //  {
-      //  bRes = false;
-      //  break;
-      //  }
+      TsWriteToViewLn("" + i + ". " + arrValue[i].strValue);
+      bLogEntry = oLog.Append(arrValue[i].strValue);
+      if (bLogEntry != arrValue[i].bResult)
+        {
+        bRes = false;
+        break;
+        }
       i++;
       }
+    }
 
+  if(bRes)
+    {
+    TsWriteToViewLn("Test CLogWnd.Create(LOG_WND)");
+    oLog.m_hLogOut = oLog.Create(LOG_WND);
+    bRes = (oLog.m_hLogOut != null);
+    }
+
+  if(bRes)
+    {
+    TsWriteToViewLn("Append new records to log window:");
+    i = 0;
+    while(i < arrValue.length)
+      {
+      var bLogEntry = true; //result of the validation
+      TsWriteToViewLn("" + i + ". " + arrValue[i].strValue);
+      bLogEntry = oLog.Append(arrValue[i].strValue);
+      if (bLogEntry != arrValue[i].bResult)
+        {
+        TsWriteToViewLn("Are pop-up windows blocked?");
+        bRes = false;
+        break;
+        }
+      i++;
+      }
     }
   }
 catch(error)
