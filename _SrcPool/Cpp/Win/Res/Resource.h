364a03d2d8946984258781d10028ac726c8bddf9
ddarko <ddarko>
2007-02-23 15:27:28 +0000

    Moved from ./Win

---
+++ _SrcPool/Cpp/Win/Res/Resource.h
@@ -0,0 +1,16 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by s:\_SrcPool\Cpp\Win\Res\Win\32\KSrcPool.rc
+//
+#define IDR_MAINFRAME                   101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         3502
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
