435fe7e2e590eaf64b6309c0aba22f23fee8df84
ddarko <ddarko>
2007-09-27 20:32:55 +0000

    Created

---
+++ _SrcPool/Cpp/KStrBool.cpp
@@ -0,0 +1,47 @@
+/*$Workfile: KBoolStream.h$: header file
+  $Revision: 3$ $Date: 2007-03-10 19:54:00$
+  $Author: Darko$
+
+  Converts a boolean number to the string.
+  Copyright: CommonSoft Inc.
+  2004-10-01 Darko Kolakovic
+*/
+
+/* Group=Strings                                                             */
+/*Note: MS VC/C++ - Disable precompiled headers (/Yu"StdAfx.h" option)       */
+
+LPCTSTR BooltoA(const bool bValue, LPCTSTR szTrue = NULL, LPCTSTR szFalse = NULL);
+
+//-----------------------------------------------------------------------------
+/*Obatins tesxtual representation of a boolean value.
+  Deafult representation for logical true is the symbol T and for logical false
+  is the symbol F:
+
+      true  T
+      false F
+
+  Example:
+    ...
+    int i = 100;
+    bool bRes = (i != 0);
+    std::_tcout << _T("i is equal to null: ") << BooltoA(bRes, "Yes", "No") << std::endl;
+    ...
+    Output: i is equal to null: Yes
+ */
+LPCTSTR BooltoA(const bool bValue,    //[in] value to write
+               LPCTSTR szTrue, //[in] = NULL representation for logical true
+               LPCTSTR szFalse //[in] = NULL representation for logical false
+               )
+{
+if (bValue)
+  return ((szTrue == NULL) ? _T("T") : szTrue);
+else
+  return ((szFalse == NULL) ? _T("F") : szFalse);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/*****************************************************************************
+ * $Log:
+ *  1    Biblioteka1.0         2004-10-01 21:51:53  Darko
+ * $
+ *****************************************************************************/
