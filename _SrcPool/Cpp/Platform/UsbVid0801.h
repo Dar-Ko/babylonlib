d89df8baa18f67bda18ecbbccf706559c822afca
ddarko <ddarko>
2008-04-08 19:32:07 +0000

    MICR

--- _SrcPool/Cpp/Platform/UsbVid0801.h
+++ _SrcPool/Cpp/Platform/UsbVid0801.h
@@ -1,5 +1,5 @@
 /*$Workfile: UsbVid0801.h$: header file
-  $Revision: 1.3 $ $Date: 2008/04/08 18:23:05 $
+  $Revision: 1.4 $ $Date: 2008/04/08 19:32:07 $
   $Author: ddarko $
 
   Universal Serial Bus (USB) vendor specific device identification
@@ -34,12 +34,18 @@
                                    21040139, 21040143, 21040140, 21040146.
                                   */
 #define USBPID_MT215MSR      0x0003 //MagTek 215/P-Series Insert Magnetic Strip Reader
+#define USBPID_MTMAGNEPRN    0x0009 //MagTek MagnePrint
+#define USBPID_MTMAGNEPRNEN  0x000E //MagTek EncMagnePrint
+#define USBPID_MTMAGNEPRNENC 0x000F //MagTek EncMagnePrintClear
+#define USBPID_MTVISAMSR     0x0010 //MagTek VISA Special Magnetic Strip Reader
+#define USBPID_MTMAGENSA4    0x0011 //MagTek Magensa V4
+#define USBPID_MTMINIMICR    0x2251 //MagTek Mini Magnetic Ink Character Recognition (MICR) Reader
 
 ///////////////////////////////////////////////////////////////////////////////
 #endif //_USBVID0801_H_
 /*****************************************************************************
  * $Log: UsbVid0801.h,v $
- * Revision 1.3  2008/04/08 18:23:05  ddarko
- * MagTek KB  Swipe
+ * Revision 1.4  2008/04/08 19:32:07  ddarko
+ * MICR
  *
  *****************************************************************************/
