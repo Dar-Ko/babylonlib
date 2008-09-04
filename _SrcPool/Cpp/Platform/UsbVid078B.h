c385d11b249afb08788a7adb03065a0f72c83eab
ddarko <ddarko>
2008-09-04 15:41:38 +0000

    Update

--- _SrcPool/Cpp/Platform/UsbVid078B.h
+++ _SrcPool/Cpp/Platform/UsbVid078B.h
@@ -1,5 +1,5 @@
 /*$Workfile: UsbVid078B.h$: header file
-  $Revision: 1.1 $ $Date: 2008/09/04 15:24:28 $
+  $Revision: 1.2 $ $Date: 2008/09/04 15:41:38 $
   $Author: ddarko $
 
   Universal Serial Bus (USB) vendor specific device identification
@@ -13,7 +13,7 @@
   //$Workfile: UsbVid078B.h$ sentry
   #define _USBVID078B_H_
 
-/*1931.	USB device vendor identification (VID) number	Suzo-Happ Group, Happ Controls, Inc	http://www.happ.com/
+/*1931.	USB device vendor identification (VID) number	Suzo-Happ Group; Happ Controls, Inc	http://www.happ.com/ ; http://www.happcontrols.com
 
   See also: usb-if.tsv (usb.if), Company List by USB Implementers Forum, Inc
   (USB-IF) at http://www.usb.org/developers/tools/comp_dump;
@@ -22,14 +22,17 @@
 #define USBVID_HAPP 0x078B
 #define USBVID_078B _T("Suzo-Happ Group") //VID 1931 Vendor name
 
-#define USBPID_DRIVING  0x0010 //Driving USB Game Control Interface (UGCI)
-#define USBPID_FLYING   0x0020 //Flying USB Game Control Interface (UGCI)
-#define USBPID_FIGHTING 0x0030 //Fighting USB Game Control Interface (UGCI)
+#define USBPID_DRIVING  0x0010 //Driving USB Game Control Interface board (UGCI Model No. 95-0800-10K)
+#define USBPID_FLYING   0x0020 //Flying USB Game Control Interface board (UGCI Model No. 95-0800-20K)
+#define USBPID_FIGHTING 0x0030 //Fighting USB Game Control Interface board (UGCI Model No. 95-0800-30K)
 
 ///////////////////////////////////////////////////////////////////////////////
 #endif //_USBVID078B_H_
 /*****************************************************************************
  * $Log: UsbVid078B.h,v $
+ * Revision 1.2  2008/09/04 15:41:38  ddarko
+ * Update
+ *
  * Revision 1.1  2008/09/04 15:24:28  ddarko
  * Created
  *
