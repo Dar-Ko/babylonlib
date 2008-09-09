a8d37425bbe59efa3b1a44783be4e3235a31224c
ddarko <ddarko>
2008-09-09 18:20:00 +0000

    ATL 8.0

--- _SrcPool/Cpp/Win/32/KDbgMacrAtl.h
+++ _SrcPool/Cpp/Win/32/KDbgMacrAtl.h
@@ -1,6 +1,6 @@
 /*$Workfile: KDbgMacrAtl.h$: header file
-  $Revision: 1$ $Date: 2007-03-06 14:36:11$
-  $Author: Darko Kolakovic$
+  $Revision: 1.3 $ $Date: 2008/09/09 18:18:48 $
+  $Author: dkolakovic $
 
   Dumps values of some Microsoft specific predefined macros
   Copyright: CommonSoft Inc.
@@ -35,30 +35,38 @@
     #endif
 
      /*Current version of Active Template Library (ATL).
+       See also: atldef.h
       */
     const int g__ATL_VER = _ATL_VER;
-    #if _ATL_VER < 0x0100
-      #pragma message ("  ver. unknown")
-    #endif
     #if _ATL_VER == 0x0100 /*Active Template Library version 1.0*/
       #pragma message ("  ver. 1.00")
+      #define DBGATL_VER 0x0100 /*Trace ATL version*/
     #endif
     #if _ATL_VER == 0x0200 /*Active Template Library version 2.0*/
       #pragma message ("  ver. 2.00")
+      #define DBGATL_VER 0x0200 /*Trace ATL version*/
     #endif
     #if _ATL_VER == 0x0300 /*Active Template Library version 3.0*/
       #pragma message ("  ver. 3.00")
+      #define DBGATL_VER 0x0300 /*Trace ATL version*/
     #endif
     #if _ATL_VER == 0x0301 /*Active Template Library version 3.0 Win64 build*/
       #pragma message ("  ver. 3.00/64-bit")
+      #define DBGATL_VER 0x0301 /*Trace ATL version*/
     #endif
     #if _ATL_VER == 0x0700
       #pragma message ("  ver. 7.00")
+      #define DBGATL_VER 0x0700 /*Trace ATL version*/
     #endif
     #if _ATL_VER == 0x0710
       #pragma message ("  ver. 7.10")
+      #define DBGATL_VER 0x0710 /*Trace ATL version*/
     #endif
-    #if _ATL_VER > 0x0710
+    #if _ATL_VER == 0x0800
+      #pragma message ("  ver. 8.00")
+      #define DBGATL_VER 0x0800 /*Trace ATL version*/
+    #endif
+    #ifndef DBGATL_VER
       #pragma message ("  ver. unknown")
     #endif
   #endif
@@ -87,6 +95,6 @@
 /* ///////////////////////////////////////////////////////////////////////// */
 #endif /*_KDBGMACRATL_H_                                                     */
 /*****************************************************************************
- * $Log: $
+ * $Log: KDbgMacrAtl.h,v $
  *****************************************************************************/
 
