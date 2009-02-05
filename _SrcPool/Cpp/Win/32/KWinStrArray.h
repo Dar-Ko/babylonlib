6560937136dbff2f51eca9368bdebd8460bd7c25
ddarko <ddarko>
2009-02-05 23:13:44 +0000

    Note about CRT library linkage

--- _SrcPool/Cpp/Win/32/KWinStrArray.h
+++ _SrcPool/Cpp/Win/32/KWinStrArray.h
@@ -1,5 +1,5 @@
 *$RCSfile: KWinStrArray.h,v $: header file
-  $Revision: 1.3 $ $Date: 2009/02/04 20:42:06 $
+  $Revision: 1.4 $ $Date: 2009/02/05 23:13:44 $
   $Author: ddarko $
 
   Array of CString objects.
@@ -33,6 +33,31 @@
   #endif
 
   #ifdef _USE_ATL  //Microsoft Active Template Library (ATL)
+    #include <atlcoll.h> //CAtlArray template
+    /*CStringAtlArray class supports arrays of CString objects.
+      CStringT is the base class for the MFC/ATL CString class.
+
+      Note: In case that STL or ATL data containers are used to exchange data
+      between dynamicaly linked library (DLL) and executibile module (EXE),
+      insure that same C-Runtime library (CRT) is used in both. If such object
+      is allocated in one module and freed in another one with different linkage
+      to CRT (and therefore different heap), following error may occur:
+          HEAP[some.exe]: Invalid Address specified to RtlValidateHeap( 003C0000, 003xxxxx )
+          Windows has triggered a breakpoint in some.exe.
+      Change project settings to use in both modules shared CRT library (/MD or /MDd
+      for debug version). For Microsoft Visual Studio 2005 change next configuration:
+          Configuration Properties | C/C++ | Code Generation | Runtime Library |
+          Multi-threaded Debug DLL (/MDd) (or /MD for release build)
+      Multi-threaded (/MT or /MTd for debug build) implies static linking to the
+      C-Runtime library.
+      Derived data types, like std::wstring or CString, manage its own memory
+      management and implemetation differs from library to library.
+      Better way is to use only basic types and to free objects where those are
+      allocated.
+     */
+    typedef CAtlArray<CString> CStringAtlArray;
+    #define _USE_STRINGATLARRAY 20090202 //Signals CStringAtlArray class usage
+
     #ifdef _USE_ATLSIMPLEARRAY
       #pragma message ("  Using ATL CSimpleArray template as CStringArray substitute.")
       #include <atlsimpcoll.h>  //CSimpleArray template
@@ -42,12 +67,14 @@
         Note: this implementation should be used when the array contains a small
         number of elements.
 
-        See also: CAtlArray
+        See also: CSimpleArray
        */
       typedef CSimpleArray<CString> CStringArray;
     #else
       #pragma message ("  Using ATL CAtlArray template as CStringArray substitute.")
-      #include <atlcoll.h> //CAtlArray template
+      #ifndef __ATLCOLL_H__
+        #include <atlcoll.h> //CAtlArray template
+      #endif
       /*CStringArray class supports arrays of CString objects.
         CStringT is the base class for the MFC/ATL CString class.
 
@@ -68,6 +95,9 @@
 #endif  //_KWINSTRARRAY_H_
 /*****************************************************************************
  * $Log: KWinStrArray.h,v $
+ * Revision 1.4  2009/02/05 23:13:44  ddarko
+ * Note about CRT library linkage
+ *
  * Revision 1.3  2009/02/04 20:42:06  ddarko
  * CSimpleArray substitute
  *
