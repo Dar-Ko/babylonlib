3395d89b3f2ac8760da1d72e667a38ecb219210d
ddarko <ddarko>
2009-05-20 20:51:38 +0000

    Clean version of USB stack

--- _SrcPool/Cpp/Peripheral/Usb200.h
+++ _SrcPool/Cpp/Peripheral/Usb200.h
@@ -1,98 +1,124 @@
-#ifndef   __USB200_H__
-#define   __USB200_H__
+/*$RCSfile: Usb200.h,v $: header file
+  $Revision: 1.2 $ $Date: 2009/05/20 20:51:38 $
+  $Author: ddarko $
 
-#include "usb100.h"
+  USB 2.0 definitions
+  Copyright: (c) 2004-2008 The Open Watcom Contributors. All Rights Reserved.
+  http://www.openwatcom.org/
+ */
+#ifndef __USB200_H__
+  #define __USB200_H__
 
+#ifndef _ENABLE_AUTODEPEND
+  #ifdef __WATCOMC__
+    /*Watcom 32b Compiler*/
+    #pragma read_only_file;
+  #endif
+#endif
 
-#include <PSHPACK1.H>
+#if __GNUC__ >= 3
+  #pragma GCC system_header
+#endif
 
+#include "Usb100.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* USB hub port status flags */
+#define USB_PORT_STATUS_CONNECT                       0x0001
+#define USB_PORT_STATUS_ENABLE                        0x0002
+#define USB_PORT_STATUS_SUSPEND                       0x0004
+#define USB_PORT_STATUS_OVER_CURRENT                  0x0008
+#define USB_PORT_STATUS_RESET                         0x0010
+#define USB_PORT_STATUS_POWER                         0x0100
+#define USB_PORT_STATUS_LOW_SPEED                     0x0200
+#define USB_PORT_STATUS_HIGH_SPEED                    0x0400
+
+/* USB descriptor types */
+#define USB_DEVICE_QUALIFIER_DESCRIPTOR_TYPE            0x06
+#define USB_OTHER_SPEED_CONFIGURATION_DESCRIPTOR_TYPE   0x07
+#define USB_INTERFACE_ASSOCIATION_DESCRIPTOR_TYPE       0x0B
+
+/*---------------------------------------------------------------------------*/
+/* All structures in this file get one-byte packing.                         */
+#include <pshpack1.h>
+
+/* USB device speeds */
 typedef enum _USB_DEVICE_SPEED {
-    UsbLowSpeed = 0,
-    UsbFullSpeed,
-    UsbHighSpeed
+    UsbLowSpeed     = 0,
+    UsbFullSpeed    = 1,
+    UsbHighSpeed    = 2
 } USB_DEVICE_SPEED;
 
+/* USB device types */
 typedef enum _USB_DEVICE_TYPE {
     Usb11Device = 0,
-    Usb20Device
+    Usb20Device = 1
 } USB_DEVICE_TYPE;
 
-// standard definiions for the port status
-// word of the HUB port register
-
-#define USB_PORT_STATUS_CONNECT         0x0001
-#define USB_PORT_STATUS_ENABLE          0x0002
-#define USB_PORT_STATUS_SUSPEND         0x0004
-#define USB_PORT_STATUS_OVER_CURRENT    0x0008
-#define USB_PORT_STATUS_RESET           0x0010
-#define USB_PORT_STATUS_POWER           0x0100
-#define USB_PORT_STATUS_LOW_SPEED       0x0200
-#define USB_PORT_STATUS_HIGH_SPEED      0x0400
-
+/* Request type */
 typedef union _BM_REQUEST_TYPE {
     struct _BM {
-        UCHAR   Recipient:2;
-        UCHAR   Reserved:3;
-        UCHAR   Type:2;
-        UCHAR   Dir:1;
-    };        
-    UCHAR B;        
-} BM_REQUEST_TYPE, *PBM_REQUEST_TYPE;
+        UCHAR   Recipient   : 2;
+        UCHAR   Reserved    : 3;
+        UCHAR   Type        : 2;
+        UCHAR   Dir         : 1;
+    };
+    UCHAR   B;
+} BM_REQUEST_TYPE;
+typedef BM_REQUEST_TYPE *PBM_REQUEST_TYPE;
 
+/* USB default pipe setup packet */
 typedef struct _USB_DEFAULT_PIPE_SETUP_PACKET {
     BM_REQUEST_TYPE bmRequestType;
-    UCHAR bRequest;
-
+    UCHAR           bRequest;
     union _wValue {
         struct {
-            UCHAR LowByte;
-            UCHAR HiByte;
+            UCHAR   LowByte;
+            UCHAR   HiByte;
         };
-        USHORT W;
+        USHORT  W;
     } wValue;
-
     union _wIndex {
         struct {
-            UCHAR LowByte;
-            UCHAR HiByte;
+            UCHAR   LowByte;
+            UCHAR   HiByte;
         };
-        USHORT W;
+        USHORT  W;
     } wIndex;
-    USHORT wLength;
-} USB_DEFAULT_PIPE_SETUP_PACKET, *PUSB_DEFAULT_PIPE_SETUP_PACKET;
+    USHORT          wLength;
+} USB_DEFAULT_PIPE_SETUP_PACKET;
+typedef USB_DEFAULT_PIPE_SETUP_PACKET   *PUSB_DEFAULT_PIPE_SETUP_PACKET;
 
-// setup packet is eight bytes -- defined by spec
-C_ASSERT(sizeof(USB_DEFAULT_PIPE_SETUP_PACKET) == 8);
-
-
-#define USB_DEVICE_QUALIFIER_DESCRIPTOR_TYPE        0x06
-
+/* USB device qualifier descriptor */
 typedef struct _USB_DEVICE_QUALIFIER_DESCRIPTOR {
-    UCHAR bLength;
-    UCHAR bDescriptorType;
-    USHORT bcdUSB;
-    UCHAR bDeviceClass;
-    UCHAR bDeviceSubClass;
-    UCHAR bDeviceProtocol;
-    UCHAR bMaxPacketSize0;
-    UCHAR bNumConfigurations;
-    UCHAR bReserved;
-} USB_DEVICE_QUALIFIER_DESCRIPTOR, *PUSB_DEVICE_QUALIFIER_DESCRIPTOR;
+    UCHAR   bLength;
+    UCHAR   bDescriptorType;
+    USHORT  bcdUSB;
+    UCHAR   bDeviceClass;
+    UCHAR   bDeviceSubClass;
+    UCHAR   bDeviceProtocol;
+    UCHAR   bMaxPacketSize0;
+    UCHAR   bNumConfigurations;
+    UCHAR   bReserved;
+} USB_DEVICE_QUALIFIER_DESCRIPTOR;
+typedef USB_DEVICE_QUALIFIER_DESCRIPTOR *PUSB_DEVICE_QUALIFIER_DESCRIPTOR;
 
-
+/* USB high speed maximum packet */
 typedef union _USB_HIGH_SPEED_MAXPACKET {
     struct _MP {
-        USHORT   MaxPacket:11;  /* 0..10 */
-        USHORT   HSmux:2;        /* 11..12 */
-        USHORT   Reserved:3;    /* 13..15 */
-    };        
-    USHORT us;     
-  } USB_HIGH_SPEED_MAXPACKET, *PUSB_HIGH_SPEED_MAXPACKET;
+        USHORT  MaxPacket   : 11;
+        USHORT  HSmux       : 2;
+        USHORT  Reserved    : 3;
+    };
+    USHORT  us;
+} USB_HIGH_SPEED_MAXPACKET;
+typedef USB_HIGH_SPEED_MAXPACKET    *PUSB_HIGH_SPEED_MAXPACKET;
 
-#define USB_INTERFACE_ASSOCIATION_DESCRIPTOR_TYPE 0x0B
-
+/* USB interface association descriptor */
 typedef struct _USB_INTERFACE_ASSOCIATION_DESCRIPTOR {
-
     UCHAR   bLength;
     UCHAR   bDescriptorType;
     UCHAR   bFirstInterface;
@@ -101,11 +127,25 @@ typedef struct _USB_INTERFACE_ASSOCIATION_DESCRIPTOR {
     UCHAR   bFunctionSubClass;
     UCHAR   bFunctionProtocol;
     UCHAR   iFunction;
+} USB_INTERFACE_ASSOCIATION_DESCRIPTOR;
+typedef USB_INTERFACE_ASSOCIATION_DESCRIPTOR    *PUSB_INTERFACE_ASSOCIATION_DESCRIPTOR;
+/*---------------------------------------------------------------------------*/
+/* Revert to default packing.                                                */
+#include <poppack.h>
 
-} USB_INTERFACE_ASSOCIATION_DESCRIPTOR, *PUSB_INTERFACE_ASSOCIATION_DESCRIPTOR;
+#ifdef __cplusplus
+} /* End of extern "C" */
+#endif
+///////////////////////////////////////////////////////////////////////////////
+#endif /* __USB200_H__ */
 
+/*****************************************************************************
+ * $Log: *
+ *****************************************************************************/
 
-#include <POPPACK.H>
-
-#endif __USB200_H__
-
+/*Open Watcom is a project of the open source community to maintain and enhance
+  the Watcom C, C++, and Fortran cross compilers and tools.
+  An Open Source license from Sybase allows free commercial and non-commercial
+  use of Open Watcom.
+  http://www.openwatcom.org/index.php/Open_Watcom_Public_License
+ */
--- _SrcPool/Cpp/Peripheral/UsbIoDef.h
+++ _SrcPool/Cpp/Peripheral/UsbIoDef.h
@@ -1,145 +1,138 @@
-/*++
+/*$RCSfile: UsbIoDef.h,v $: header file
+  $Revision: 1.2 $ $Date: 2009/05/20 20:51:38 $
+  $Author: ddarko $
 
-Copyright (c) Microsoft Corporation. All rights reserved.
+  USB I/O definitions
+  Copyright: (c) 2004-2008 The Open Watcom Contributors. All Rights Reserved.
+  http://www.openwatcom.org/
+ */
+#ifndef __USBIODEF_H__
+  #define __USBIODEF_H__
 
-Module Name:
+#ifndef _ENABLE_AUTODEPEND
+  #ifdef __WATCOMC__
+    /*Watcom 32b Compiler*/
+    #pragma read_only_file;
+  #endif
+#endif
 
-        USBIODEF.H
+#if __GNUC__ >= 3
+  #pragma GCC system_header
+#endif
 
-Abstract:
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-        Common header file for all USB IOCTLs defined for
-        the core stack.  We define them in this single header file
-        so that we can maintain backward compatibilty with older
-        versions of the stack.
+/* USB I/O control indices */
+#define USB_SUBMIT_URB                            0
+#define USB_RESET_PORT                            1
+#define USB_GET_ROOTHUB_PDO                       3
+#define USB_GET_PORT_STATUS                       4
+#define USB_ENABLE_PORT                           5
+#define USB_GET_HUB_COUNT                         6
+#define USB_CYCLE_PORT                            7
+#define USB_GET_HUB_NAME                          8
+#define USB_IDLE_NOTIFICATION                     9
+#define USB_RECORD_FAILURE                       10
+#define USB_GET_BUS_INFO                        264
+#define USB_GET_CONTROLLER_NAME                 265
+#define USB_GET_BUSGUID_INFO                    266
+#define USB_GET_PARENT_HUB_INFO                 267
+#define USB_GET_DEVICE_HANDLE                   268
+#define USB_GET_DEVICE_HANDLE_EX                269
+#define USB_GET_TT_DEVICE_HANDLE                270
+#define USB_GET_TOPOLOGY_ADDRESS                271
+#define USB_IDLE_NOTIFICATION_EX                272
+#define USB_REQ_GLOBAL_SUSPEND                  273
+#define USB_REQ_GLOBAL_RESUME                   274
+#define USB_GET_HUB_CONFIG_INFO                 275
+#define HCD_GET_STATS_1                         255
+#define HCD_DIAGNOSTIC_MODE_ON                  256
+#define HCD_DIAGNOSTIC_MODE_OFF                 257
+#define HCD_GET_ROOT_HUB_NAME                   258
+#define HCD_GET_DRIVERKEY_NAME                  265
+#define HCD_GET_STATS_2                         266
+#define HCD_DISABLE_PORT                        268
+#define HCD_ENABLE_PORT                         269
+#define HCD_USER_REQUEST                        270
+#define HCD_TRACE_READ_REQUEST                  275
+#define USB_GET_NODE_INFORMATION                258
+#define USB_GET_NODE_CONNECTION_INFORMATION     259
+#define USB_GET_DESCRIPTOR_FROM_NODE_CONNECTION 260
+#define USB_GET_NODE_CONNECTION_NAME            261
+#define USB_DIAG_IGNORE_HUBS_ON                 262
+#define USB_DIAG_IGNORE_HUBS_OFF                263
+#define USB_GET_NODE_CONNECTION_DRIVERKEY_NAME  264
+#define USB_GET_HUB_CAPABILITIES                271
+#define USB_GET_NODE_CONNECTION_ATTRIBUTES      272
+#define USB_HUB_CYCLE_PORT                      273
+#define USB_GET_NODE_CONNECTION_INFORMATION_EX  274
+#define USB_RESET_HUB                           275
+#define USB_GET_HUB_CAPABILITIES_EX             276
 
+/* USB device type */
+#define FILE_DEVICE_USB FILE_DEVICE_UNKNOWN
 
-        We divide the IOCTLS supported by the stack as follows:
+/* Macro to make USB I/O control codes */
+#define USB_CTL( x ) \
+    CTL_CODE( FILE_DEVICE_USB, x, METHOD_BUFFERED, FILE_ANY_ACCESS )
+#define USB_KERNEL_CTL( x ) \
+    CTL_CODE( FILE_DEVICE_USB, x, METHOD_NEITHER, FILE_ANY_ACCESS )
+#define USB_KERNEL_CTL_BUFFERED( x ) \
+    CTL_CODE( FILE_DEVICE_USB, x, METHOD_BUFFERED, FILE_ANY_ACCESS )
 
-        kernel IOCTLS:
+/* USB idle callback */
+#if (_WIN32_WINNT >= 0x0501)
+typedef VOID (NTAPI *USB_IDLE_CALLBACK)( PVOID );
+#endif
 
+/* USB idle callback information */
+#if (_WIN32_WINNT >= 0x0501)
+typedef struct _USB_IDLE_CALLBACK_INFO {
+    USB_IDLE_CALLBACK   IdleCallback;
+    PVOID               IdleContext;
+} USB_IDLE_CALLBACK_INFO;
+typedef USB_IDLE_CALLBACK_INFO  *PUSB_IDLE_CALLBACK_INFO;
+#endif
 
-        user IOCTLS:
+#ifdef __cplusplus
+} /* End of extern "C" */
+#endif
+///////////////////////////////////////////////////////////////////////////////
+#endif /* __USBIODEF_H__ */
 
-            IOCTLS Handled by HCD (PORT) FDO
-            IOCTLS Handled by HUB FDO
-            IOCTLS Handled by USB (DEVICE) PDO
+/*****************************************************************************
+ * $Log: *
+ *****************************************************************************/
 
-Environment:
+/*Copyright (c) Microsoft Corporation. All rights reserved.
+  Common header file for all USB IOCTLs defined for the core stack.
+  We divide the IOCTLS supported by the stack as follows:
+   - kernel IOCTLS:
+   - user IOCTLS:
+      * IOCTLS Handled by HCD (PORT) FDO
+      * IOCTLS Handled by HUB FDO
+      * IOCTLS Handled by USB (DEVICE) PDO
+ */
 
-    kernel & user mode
+/*Contributors:
+ *   Created by Casper S. Hornstrup <chorns@users.sourceforge.net>
+ *
+ * THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ * This source code is offered for use in the public domain. You may
+ * use, modify or distribute it freely.
+ *
+ * This code is distributed in the hope that it will be useful but
+ * WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ * DISCLAIMED. This includes but is not limited to warranties of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
-Revision History:
-
-
---*/
-
-#ifndef   __USBIODEF_H__
-#define   __USBIODEF_H__
-
-/*
-    kernel mode IOCTL index values
-
-    The following codes are valid only if passed as in
-    the icControlCode parameter for
-    IRP_MJ_INTERNAL_DEVICE_CONTROL
-
-*/
-#define USB_SUBMIT_URB              0
-#define USB_RESET_PORT              1
-#define USB_GET_ROOTHUB_PDO         3
-#define USB_GET_PORT_STATUS         4
-#define USB_ENABLE_PORT             5
-#define USB_GET_HUB_COUNT           6
-#define USB_CYCLE_PORT              7
-#define USB_GET_HUB_NAME            8
-#define USB_IDLE_NOTIFICATION       9
-#define USB_GET_BUS_INFO            264
-#define USB_GET_CONTROLLER_NAME     265
-#define USB_GET_BUSGUID_INFO        266
-#define USB_GET_PARENT_HUB_INFO     267
-#define USB_GET_DEVICE_HANDLE       268
-
-
-/*
-    user mode IOCTL index values
-
-    The following codes are valid only if passed as in
-    the icControlCode parameter for
-    IRP_MJ_DEVICE_CONTROL
-    hence, they are callable by user mode applications
-*/
-#define HCD_GET_STATS_1                     255
-#define HCD_DIAGNOSTIC_MODE_ON              256
-#define HCD_DIAGNOSTIC_MODE_OFF             257
-#define HCD_GET_ROOT_HUB_NAME               258
-#define HCD_GET_DRIVERKEY_NAME              265
-#define HCD_GET_STATS_2                     266
-#define HCD_DISABLE_PORT                    268
-#define HCD_ENABLE_PORT                     269
-#define HCD_USER_REQUEST                    270
-
-#define USB_GET_NODE_INFORMATION                    258
-#define USB_GET_NODE_CONNECTION_INFORMATION         259
-#define USB_GET_DESCRIPTOR_FROM_NODE_CONNECTION     260
-#define USB_GET_NODE_CONNECTION_NAME                261
-#define USB_DIAG_IGNORE_HUBS_ON                     262
-#define USB_DIAG_IGNORE_HUBS_OFF                    263
-#define USB_GET_NODE_CONNECTION_DRIVERKEY_NAME      264
-#define USB_GET_HUB_CAPABILITIES                    271
-#define USB_GET_NODE_CONNECTION_ATTRIBUTES          272
-#define	USB_HUB_CYCLE_PORT							273
-#define USB_GET_NODE_CONNECTION_INFORMATION_EX      274
-
-/*
-USB specific GUIDs
-*/
-
-
-/* f18a0e88-c30c-11d0-8815-00a0c906bed8 */
-DEFINE_GUID(GUID_DEVINTERFACE_USB_HUB,    0xf18a0e88, 0xc30c, 0x11d0, 0x88, 0x15, 0x00, \
-             0xa0, 0xc9, 0x06, 0xbe, 0xd8);
-
-/* A5DCBF10-6530-11D2-901F-00C04FB951ED */
-DEFINE_GUID(GUID_DEVINTERFACE_USB_DEVICE, 0xA5DCBF10L, 0x6530, 0x11D2, 0x90, 0x1F, 0x00, \
-             0xC0, 0x4F, 0xB9, 0x51, 0xED);
-
-/* 3ABF6F2D-71C4-462a-8A92-1E6861E6AF27 */
-DEFINE_GUID(GUID_DEVINTERFACE_USB_HOST_CONTROLLER, 0x3abf6f2d, 0x71c4, 0x462a, 0x8a, 0x92, 0x1e, \
-             0x68, 0x61, 0xe6, 0xaf, 0x27);
-             
-/* 4E623B20-CB14-11D1-B331-00A0C959BBD2 */
-DEFINE_GUID(GUID_USB_WMI_STD_DATA, 0x4E623B20L, 0xCB14, 0x11D1, 0xB3, 0x31, 0x00,\
-             0xA0, 0xC9, 0x59, 0xBB, 0xD2);
-
-/* 4E623B20-CB14-11D1-B331-00A0C959BBD2 */
-DEFINE_GUID(GUID_USB_WMI_STD_NOTIFICATION, 0x4E623B20L, 0xCB14, 0x11D1, 0xB3, 0x31, 0x00,\
-             0xA0, 0xC9, 0x59, 0xBB, 0xD2);        
-
-
-/*
-Obsolete device interface class GUID names.
-(use of above GUID_DEVINTERFACE_* names is recommended).
---*/
-
-#define GUID_CLASS_USBHUB               GUID_DEVINTERFACE_USB_HUB
-#define GUID_CLASS_USB_DEVICE           GUID_DEVINTERFACE_USB_DEVICE
-#define GUID_CLASS_USB_HOST_CONTROLLER  GUID_DEVINTERFACE_USB_HOST_CONTROLLER
-
-#define FILE_DEVICE_USB         FILE_DEVICE_UNKNOWN
-
-/*
-    common macro used by IOCTL header files
-*/
-#define USB_CTL(id)  CTL_CODE(FILE_DEVICE_USB,  \
-                                      (id), \
-                                      METHOD_BUFFERED,  \
-                                      FILE_ANY_ACCESS)
-
-#define USB_KERNEL_CTL(id)  CTL_CODE(FILE_DEVICE_USB,  \
-                                      (id), \
-                                      METHOD_NEITHER,  \
-                                      FILE_ANY_ACCESS)                                      
-
-#endif //__USBIODEF_H__
-
+/*Open Watcom is a project of the open source community to maintain and enhance
+  the Watcom C, C++, and Fortran cross compilers and tools.
+  An Open Source license from Sybase allows free commercial and non-commercial
+  use of Open Watcom.
+  http://www.openwatcom.org/index.php/Open_Watcom_Public_License
+ */
