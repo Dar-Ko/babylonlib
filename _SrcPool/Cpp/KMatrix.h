232220272ea1eccb7c98d4e772226c20b62440ab
ddarko <ddarko>
2009-07-15 16:04:45 +0000

    Reformatted code

--- _SrcPool/Cpp/KMatrix.h
+++ _SrcPool/Cpp/KMatrix.h
@@ -1,68 +1,65 @@
-// Matrix.h
+/*$RCSfile: KMatrix.h,v $: header file
+  $Revision: 1.2 $ $Date: 2009/07/15 16:04:45 $
+  $Author: ddarko $
 
-#ifndef H__MATRIX
-#define H__MATRIX 1
+  Basic matrix operations
+  1998-12-02 Philip J. Erdelsky
+ */
 
-#include <iostream.h>
+#ifndef _KMATRIX_H_
+    //$RCSfile: KMatrix.h,v $ sentry
+  #define _KMATRIX_H_
+
+#ifdef _DEBUG_INCL_PREPROCESS   //Preprocessor: debugging included files
+  #pragma message ("   #include " __FILE__ )
+#endif
+
+#ifdef __cplusplus
+
 #include <mem.h>
 
-/*Matrix Package
-Title: 
-Matrix Package 
-Language: 
-C++ 
-Author: 
-Philip J. Erdelsky 
-pje@acm.org 
-http://www.alumni.caltech.edu/~pje/ 
-Date: 
-December 2, 1998 
-Usage: 
-Public domain; no restrictions on use 
-Portability: 
-Any C++ compiler that implements templates, exceptions and properties 
-Keywords: 
-Matrix 
-Abstract: 
-A C++ package to perform basic matrix operations 
-Source: 
-matrix.h 
-This package performs all of the most common matrix operations for rather arbitrary matrices. The package is implemented as a template class, in which the type of the matrix elements is a parameter. 
+////////////////////////////////////////////////////////////////////////////////
+/*This package performs all of the most common TMatrix operations for rather
+  arbitrary matrices. The package is implemented as a template class, in which
+  the type of the TMatrix elements is a parameter.
 
-The file MATRIX.H includes the entire package. It must be included in, or copied into, any source file that uses the package. 
 
-To define a matrix, first choose a type for the matrix elements. Usually, this is float or double. It must be a type which implements the usual aritmetic operations: addition, subtraction, multiplication, division and comparison. 
+  To define a TMatrix, first choose a type for the TMatrix elements.
+  Usually, this is float or double. It must be a type which implements
+  the usual aritmetic operations: addition, subtraction, multiplication,
+  division and comparison.
 
-Then define a matrix type as follows: 
+  Then define a TMatrix type as follows:
 
-     general form:  typedef matrix<T> mymatrix;
+     general form:  typedef TMatrix<TYPE> mymatrix;
 
-     example:       typedef matrix<float> mymatrix;
+     example:       typedef TMatrix<float> mymatrix;
 
-There are two constructors: 
+  There are two constructors:
 
      mymatrix A(height, width);
 
-     int height;     number of rows in matrix
-     int width ;     number of columns in matrix
+     int height;     number of rows in TMatrix
+     int width ;     number of columns in TMatrix
 
      mymatrix A(B);
 
-     mymatrix B;     matrix to be copied into A
+     mymatrix B;     TMatrix to be copied into A
 
-Notice that the dimensions of a matrix are determined when it is constructed and cannot be changed dynamically. 
+  Notice that the dimensions of a TMatrix are determined when it is constructed
+  and cannot be changed dynamically.
 
-The usual matrix operations are implemented as overloaded operators: 
+  The usual TMatrix operations are implemented as overloaded operators:
 
      A + B
      A - B
      - A
      A * B
-     x * B   x is a scalar of the same type as matrix elements
+     x * B   x is a scalar of the same type as TMatrix elements
      A = B   assignment of matrices
      A == B  comparison of matrices
 
-Other operations are as follows: 
+  Other operations are as follows:
 
     A.inverse
     A.transpose
@@ -71,299 +68,386 @@ Other operations are as follows:
     A(i,j)       element in i-th row and j-th column of A, where
                  the upper left element is A(1,1)
 
-    out << A     edit and send a matrix to an output stream
+    out << A     edit and send a TMatrix to an output stream
 
-    in >> A      read a matrix from an input stream
+    in >> A      read a TMatrix from an input stream
 
-A static function returns an identity matrix of the specified size: 
+A   static function returns an identity TMatrix of the specified size:
 
-    mymatrix::I(N)   returns N X N identity matrix
+    mymatrix::I(N)   returns N X N identity TMatrix
 
-Both the input and output operations process the elements in lexicographical order, i.e., the order in which English text is usually read when the matrix elements are in their usual positions. 
+  Both the input and output operations process the elements in lexicographical
+  order, i.e., the order in which English text is usually read when the TMatrix
+  elements are in their usual positions.
 
-If there is an error, the package will throw an exception called mymatrix::exception, which has a member that explains the error. You can catch the exception as follows: 
+  If there is an error, the package will throw an exception called
+  mymatrix::exception, which has a member that explains the error.
+  You can catch the exception as follows:
 
      try
      {
        ...
-       matrix operations
+       TMatrix operations
      }
      catch (mymatrix::exception &E)
      {
        cout << E.message;
      }
 
-Expressions such as A.height, A.width, A.transpose and A.inverse are implemented as properties. If the C++ compiler does not support properties, slight changes will convert them to member functions. 
+  Expressions such as A.height, A.width, A.transpose and A.inverse are
+  implemented as properties. If the C++ compiler does not support properties,
+  slight changes will convert them to member functions.
+
+Parameters:
+  - TYPE the type of objects stored in the matrix.
+
 */
-template <class T> class matrix
+template <class TYPE> class TMatrix
 {
-  private:
-    int h, w;
-    T *cells;
-    matrix trans(void) const;
-    matrix inv(void) const;
-  public:
-    matrix(int height, int width);
-    matrix(const matrix &);
-    void operator = (matrix);
-    ~matrix();
-    __property int height = {read=h};
-    __property int width = {read=w};
-    __property matrix transpose = {read=trans};
-    __property matrix inverse = {read=inv};
-    T & operator () (int, int) const;
-    class exception
+public:
+  TMatrix(int height, int width);
+  TMatrix(const TMatrix &);
+  void operator = (TMatrix);
+  ~TMatrix();
+  __property int height = {read=h};
+  __property int width = {read=w};
+  __property TMatrix transpose = {read=trans};
+  __property TMatrix inverse = {read=inv};
+  TYPE & operator () (int, int) const;
+
+private:
+  TMatrix trans(void) const;
+  TMatrix inv(void) const;
+  static TMatrix I(int);
+
+private:
+  int h, w;
+  TYPE *cells;
+
+  class exception
     {
-      public:
+    public:
       const char *message;
       exception(const char *s) {message = s;}
     };
-    static matrix I(int);
 };
 
-template <class T> istream & operator >> (istream &in, matrix<T> &A)
+///////////////////////////////////////////////////////////////////////////////
+// Inlines
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE>::TMatrix(int height, int width)
 {
-  for (int i = 1; i <= A.height; i++)
-  {
-    for (int j = 1; j <= A.width; j++)
-      in >> A(i,j);
-  }
-  return in;
+h = height;
+w = width;
+int n = h*w;
+cells = new TYPE[n];
 }
 
-template <class T> ostream & operator << (ostream &out, matrix<T> A)
+/*
+ */
+template <class TYPE> TMatrix<TYPE>::TMatrix(const TMatrix<TYPE> &A)
 {
+h = A.height;
+w = A.width;
+cells = new TYPE[h*w];
+memcpy(cells, A.cells, h*w*sizeof(TYPE));
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE>::~TMatrix()
+{
+delete cells;
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> void TMatrix<TYPE>::operator = (TMatrix<TYPE> A)
+{
+if (h != A.height || w != A.width)
+  throw TMatrix<TYPE>::exception("Incompatible TMatrix dimensions");
+h = A.height;
+w = A.width;
+memcpy(cells, A.cells, h*w*sizeof(TYPE));
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TYPE &TMatrix<TYPE>::operator () (int row, int column) const
+{
+return cells[(row-1)*w + column - 1];
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE> TMatrix<TYPE>::trans(void) const
+{
+TMatrix<TYPE> TYPE(width, height);
+int i;
+for (i = 1; i <= TYPE.height; i++)
+  {
+  int j;
+  for (j = 1; j <= TYPE.width; j++)
+    TYPE(i,j) = (*this)(j,i);
+  }
+return TYPE;
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE> TMatrix<TYPE>::I(int n)
+{
+if (n < 1)
+  n = 1;
+TMatrix<TYPE> U(n, n);
+int i;
+for (i = 1; i <= n; i++)
+  U(i,i) = 1.0;
+return U;
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE> TMatrix<TYPE>::inv(void) const
+{
+if (height != width)
+  throw exception("Inversion of non-square TMatrix");
+const int N = height;
+TMatrix<TYPE> B(*this), R(N,N);
+R = I(N);
+int k;
+for (k = 1; k <= N; k++)
+  {
+  int pivot_row = 0;
+  TYPE pivot_value;
+  TYPE abs_pivot_value;
+  int i;
+  for (i = k; i <= N; i++)
+    {
+    TYPE x = B(i,k);
+    TYPE absx = x;
+    if (absx < 0)
+      absx = -absx;
+    if (pivot_row == 0 || absx > abs_pivot_value)
+      {
+      pivot_row = i;
+      abs_pivot_value = absx;
+      pivot_value = x;
+      }
+    if (pivot_value == 0.0)
+      throw exception("Inversion of singular TMatrix");
+    }
+  int j;
+  if (pivot_row == k)
+    {
+    for (j = k; j <= N; j++)
+      B(k,j) = B(k,j) / pivot_value;
+    for (j = 1; j <= N; j++)
+      R(k,j) = R(k,j) / pivot_value;
+    }
+  else
+    {
+    for (j = k; j <= N; j++)
+      {
+      TYPE x = B(k,j);
+      B(k,j) = B(pivot_row,j) / pivot_value;
+      B(pivot_row,j) = x;
+      }
+    for (j = 1; j <= N; j++)
+      {
+      TYPE x = R(k,j);
+      R(k,j) = R(pivot_row,j) / pivot_value;
+      R(pivot_row,j) = x;
+      }
+    }
+  for (i = 1; i <= N; i++)
+    {
+    if (i != k)
+      {
+      int j;
+      for (j = k+1; j <= N; j++)
+        B(i,j) = B(i,j) - B(i,k) * B(k,j);
+      for (j = 1; j <= N; j++)
+        R(i,j) = R(i,j) - B(i,k) * R(k,j);
+      }
+    }
+  }
+return R;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE> operator + (TMatrix<TYPE> A, TMatrix<TYPE> B)
+{
+if (A.height != B.height || A.width != B.width)
+  throw TMatrix<TYPE>::exception("Incompatible TMatrix dimensions");
+int i;
+for (i = 1; i <= A.height; i++)
+  {
+  int j;
+  for (j = 1; j <= A.width; j++)
+    A(i,j) = A(i,j) + B(i,j);
+  }
+return A;
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE> operator - (TMatrix<TYPE> A)
+{
+int i;
+for (i = 1; i <= A.height; i++)
+  {
+  int j;
+  for (j = 1; j <= A.width; j++)
+    A(i,j) = -A(i,j);
+  }
+return A;
+}
+
+/*
+ */
+template <class TYPE> TMatrix<TYPE> operator - (TMatrix<TYPE> A, TMatrix<TYPE> B)
+{
+if (A.height != B.height || A.width != B.width)
+  throw TMatrix<TYPE>::exception("Incompatible TMatrix dimensions");
+int i;
+for (i = 1; i <= A.height; i++)
+  {
+  int j;
+  for (j = 1; j <= A.width; j++)
+    A(i,j) = A(i,j) - B(i,j);
+  }
+return A;
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> TMatrix<TYPE> operator * (TYPE x, TMatrix<TYPE> A)
+{
+int i;
+for (i = 1; i <= A.height; i++)
+  {
+  int j;
+  for (j = 1; j <= A.width; j++)
+    A(i,j) = x * A(i,j);
+  }
+return A;
+}
+
+/*
+ */
+template <class TYPE> TMatrix<TYPE> operator* (TMatrix<TYPE> A, TMatrix<TYPE> B)
+{
+if (A.width != B.height)
+  throw TMatrix<TYPE>::exception("Incompatible TMatrix dimensions");
+int i;
+TMatrix<TYPE> AB(A.height, B.width);
+for (i = 1; i <= AB.height; i++)
+  {
+  int j;
+  for (j = 1; j <= AB.width; j++)
+    {
+    AB(i,j) = 0.0;
+    int k;
+    for (k = 1; k <= A.width; k++)
+      AB(i,j) = AB(i,j) + A(i,k) * B(k,j);
+    }
+  }
+return AB;
+}
+
+//-----------------------------------------------------------------------------
+/*
+ */
+template <class TYPE> bool operator == (TMatrix<TYPE> A, TMatrix<TYPE> B)
+{
+if (A.height != B.height || A.width != B.width)
+  throw TMatrix<TYPE>::exception("Incompatible TMatrix dimensions");
+int i;
+for (i = 1; i <= A.height; i++)
+  {
+  int j;
+  for (j = 1; j <= A.width; j++)
+    {
+    if (!(A(i,j) == B(i,j)))
+      return false;
+    }
+  }
+return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef  _USE_STL //Standard Template Library (STL)
+
+  #include <iostream>
+
+  //----------------------------------------------------------------------------
+  /*
+   */
+  template <class TYPE> istream & operator >> (istream &in, TMatrix<TYPE> &A)
+  {
+  for (int i = 1; i <= A.height; i++)
+    {
+    for (int j = 1; j <= A.width; j++)
+      in >> A(i,j);
+    }
+  return in;
+  }
+
+  //----------------------------------------------------------------------------
+  /*
+   */
+  template <class TYPE> ostream & operator << (ostream &out, TMatrix<TYPE> A)
+  {
   int width = out.width();
   int precision = out.precision();
   long flags = out.flags();
   for (int i = 1; i <= A.height; i++)
-  {
-    for (int j = 1; j <= A.width; j++)
     {
+    for (int j = 1; j <= A.width; j++)
+      {
       out.width(width);
       out.precision(precision);
       out.flags(flags);
       out << A(i,j);
-    }
+      }
     out << endl;
-  }
+    }
   return out;
-}
-
-template <class T> matrix<T>::matrix(int height, int width)
-{
-  h = height;
-  w = width;
-  int n = h*w;
-  cells = new T[n];
-}
-
-template <class T> matrix<T>::matrix(const matrix<T> &A)
-{
-  h = A.height;
-  w = A.width;
-  cells = new T[h*w];
-  memcpy(cells, A.cells, h*w*sizeof(T));
-}
-
-template <class T> void matrix<T>::operator = (matrix<T> A)
-{
-  if (h != A.height || w != A.width)
-    throw matrix<T>::exception("Incompatible matrix dimensions");
-  h = A.height;
-  w = A.width;
-  memcpy(cells, A.cells, h*w*sizeof(T));
-}
-
-template <class T> matrix<T>::~matrix()
-{
-  delete cells;
-}
-
-template <class T> T &matrix<T>::operator () (int row, int column) const
-{
-  return cells[(row-1)*w + column - 1];
-}
-
-template <class T> matrix<T> operator + (matrix<T> A, matrix<T> B)
-{
-  if (A.height != B.height || A.width != B.width)
-    throw matrix<T>::exception("Incompatible matrix dimensions");
-  int i;
-  for (i = 1; i <= A.height; i++)
-  {
-    int j;
-    for (j = 1; j <= A.width; j++)
-      A(i,j) = A(i,j) + B(i,j);
   }
-  return A;
-}
+#endif //_USE_STL
+///////////////////////////////////////////////////////////////////////////////
 
-template <class T> bool operator == (matrix<T> A, matrix<T> B)
-{
-  if (A.height != B.height || A.width != B.width)
-    throw matrix<T>::exception("Incompatible matrix dimensions");
-  int i;
-  for (i = 1; i <= A.height; i++)
-  {
-    int j;
-    for (j = 1; j <= A.width; j++)
-    {
-      if (!(A(i,j) == B(i,j)))
-        return false;
-    }
-  }
-  return true;
-}
+#endif //__cplusplus
+#endif  //_KMATRIX_H_
+/*****************************************************************************
+ * $Log: KMatrix.h,v $
+ * Revision 1.2  2009/07/15 16:04:45  ddarko
+ * Reformatted code
+ *
+ *****************************************************************************/
 
-template <class T> matrix<T> operator - (matrix<T> A)
-{
-  int i;
-  for (i = 1; i <= A.height; i++)
-  {
-    int j;
-    for (j = 1; j <= A.width; j++)
-      A(i,j) = -A(i,j);
-  }
-  return A;
-}
-
-template <class T> matrix<T> operator * (T x, matrix<T> A)
-{
-  int i;
-  for (i = 1; i <= A.height; i++)
-  {
-    int j;
-    for (j = 1; j <= A.width; j++)
-      A(i,j) = x * A(i,j);
-  }
-  return A;
-}
-
-template <class T> matrix<T> operator - (matrix<T> A, matrix<T> B)
-{
-  if (A.height != B.height || A.width != B.width)
-    throw matrix<T>::exception("Incompatible matrix dimensions");
-  int i;
-  for (i = 1; i <= A.height; i++)
-  {
-    int j;
-    for (j = 1; j <= A.width; j++)
-      A(i,j) = A(i,j) - B(i,j);
-  }
-  return A;
-}
-
-template <class T> matrix<T> operator * (matrix<T> A, matrix<T> B)
-{
-  if (A.width != B.height)
-    throw matrix<T>::exception("Incompatible matrix dimensions");
-  int i;
-  matrix<T> AB(A.height, B.width);
-  for (i = 1; i <= AB.height; i++)
-  {
-    int j;
-    for (j = 1; j <= AB.width; j++)
-    {
-      AB(i,j) = 0.0;
-      int k;
-      for (k = 1; k <= A.width; k++)
-        AB(i,j) = AB(i,j) + A(i,k) * B(k,j);
-    }
-  }
-  return AB;
-}
-
-template <class T> matrix<T> matrix<T>::trans(void) const
-{
-  matrix<T> T(width, height);
-  int i;
-  for (i = 1; i <= T.height; i++)
-  {
-    int j;
-    for (j = 1; j <= T.width; j++)
-      T(i,j) = (*this)(j,i);
-  }
-  return T;
-}
-
-template <class T> matrix<T> matrix<T>::I(int n)
-{
-  if (n < 1)
-    n = 1;
-  matrix<T> U(n, n);
-  int i;
-  for (i = 1; i <= n; i++)
-    U(i,i) = 1.0;
-  return U;
-}
-
-template <class T> matrix<T> matrix<T>::inv(void) const
-{
-  if (height != width)
-    throw exception("Inversion of non-square matrix");
-  const int N = height;
-  matrix<T> B(*this), R(N,N);
-  R = I(N);
-  int k;
-  for (k = 1; k <= N; k++)
-  {
-    int pivot_row = 0;
-    T pivot_value;
-    T abs_pivot_value;
-    int i;
-    for (i = k; i <= N; i++)
-    {
-      T x = B(i,k);
-      T absx = x;
-      if (absx < 0)
-        absx = -absx;
-      if (pivot_row == 0 || absx > abs_pivot_value)
-      {
-        pivot_row = i;
-        abs_pivot_value = absx;
-        pivot_value = x;
-      }
-      if (pivot_value == 0.0)
-        throw exception("Inversion of singular matrix");
-    }
-    int j;
-    if (pivot_row == k)
-    {
-      for (j = k; j <= N; j++)
-        B(k,j) = B(k,j) / pivot_value;
-      for (j = 1; j <= N; j++)
-        R(k,j) = R(k,j) / pivot_value;
-    }
-    else
-    {
-      for (j = k; j <= N; j++)
-      {
-        T x = B(k,j);
-        B(k,j) = B(pivot_row,j) / pivot_value;
-        B(pivot_row,j) = x;
-      }
-      for (j = 1; j <= N; j++)
-      {
-        T x = R(k,j);
-        R(k,j) = R(pivot_row,j) / pivot_value;
-        R(pivot_row,j) = x;
-      }
-    }
-    for (i = 1; i <= N; i++)
-    {
-      if (i != k)
-      {
-        int j;
-        for (j = k+1; j <= N; j++)
-          B(i,j) = B(i,j) - B(i,k) * B(k,j);
-        for (j = 1; j <= N; j++)
-          R(i,j) = R(i,j) - B(i,k) * R(k,j);
-      }
-    }
-  }
-  return R;
-}
-
-#endif
-
+/*Author: Philip J. Erdelsky; pje@acm.org; http://www.alumni.caltech.edu/~pje/
+  Date:        December 2, 1998
+  Usage:       Public domain; no restrictions on use
+  Portability: Any C++ compiler that implements templates, exceptions and properties
+  Keywords:    Matrix
+  Abstract:    A C++ package to perform basic TMatrix operations
+ */
