9b71eea006c4b1e7de4084c522f994cd9bc6a6da
ddarko <ddarko>
2009-09-29 21:24:29 +0000

    fixed typo

--- _SrcPool/Cpp/Samples/StdLib/Src/TestText.c
+++ _SrcPool/Cpp/Samples/StdLib/Src/TestText.c
@@ -1,5 +1,5 @@
 /*$RCSfile: TestText.c,v $: implementation file
-  $Revision: 1.1 $ $Date: 2009/09/29 18:53:05 $
+  $Revision: 1.2 $ $Date: 2009/09/29 21:24:29 $
   $Author: ddarko $
 
   Global test samples
@@ -12,19 +12,19 @@
 /*Note: MS VC/C++ - Disable precompiled headers (/Yu"StdAfx.h" option)       */
 #include "stdafx.h"
 
-/*Single-byte cahcarcter set (SBCS) text samples
+/*Single-byte character set (SBCS) text samples
  */
 const char* g_listTestStringsA[] =
   {
-   "This ia a longish text string, but not that long, at that",
-   "The long jump \0 and some trailing text at-that",
-   "",
-   "Some more test strings",
-   "Yet another one",
-   "what-finally-stops--at-at-that point",
-   "aksdy923492p4  p39475 q0349750[q3hf30q79340 320496t034 03q495609324uy509y",
-   "character is considered to be part of the string. The chr at-that point",
-   0
+  "This is a longish text string, but not that long, at that",
+  "The long jump \0 and some trailing text at-that",
+  "",
+  "Some more test strings",
+  "Yet another one",
+  "what-finally-stops--at-at-that point",
+  "aksdy923492p4  p39475 q0349750[q3hf30q79340 320496t034 03q495609324uy509y",
+  "character is considered to be part of the string. The chr at-that point",
+  NULL   //list terminator
   };
 
 /*******************************************************************************
