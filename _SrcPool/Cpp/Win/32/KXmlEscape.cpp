6c8794c980ec579c0766c42903182fb85b3e71b6
ddarko <ddarko>
2009-10-08 21:36:06 +0000

    moved to root

--- _SrcPool/Cpp/Win/32/KXmlEscape.cpp
+++ _SrcPool/Cpp/Win/32/KXmlEscape.cpp
@@ -1,4 +1,4 @@
-/*$Workfile: KXmlEscape.cpp$: implementation file
+/*$RCSfile: KXmlEscape.cpp$: implementation file
   $Revision: 1.1 $ $Date: 2007/09/14 12:40:16 $
   $Author: dkolakovic $
 
@@ -35,14 +35,10 @@
 
   Returns String value containing escaped characters.
 
-  Note: uses Microsoft Foundation Library (MFC) or
-        uses Microsoft Active Template Library (ATL);
-        Microsoft Windows specific (Win32).
-
   See also: VB Function XmlEscape, Function XmlUnescape,
   Boost STL class xml_escape,class xml_unescape
 */
-CString XmlEscape(CString& strData //[in]
+CString XmlEscape(CString& strData //[in] text to be converted to  the XML format
                  )
 {
 strData.Replace(_T("&"),  _T("&amp;"));
