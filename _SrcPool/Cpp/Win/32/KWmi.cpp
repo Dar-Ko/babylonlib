8f31e4ab8bfa37dbb3c63f95e49db20c349e66ca
ddarko <ddarko>
2010-01-27 22:41:27 +0000

    *** empty log message ***

--- _SrcPool/Cpp/Win/32/KWmi.cpp
+++ _SrcPool/Cpp/Win/32/KWmi.cpp
@@ -1,44 +1,54 @@
 /*$RCSfile: KWmi.cpp,v $: implementation file
-  $Revision: 1.1 $ $Date: 2010/01/26 22:47:05 $
+  $Revision: 1.2 $ $Date: 2010/01/27 22:41:27 $
   $Author: ddarko $
 
   Microsoft Windows Management Instrumentation (WMI) client.
   Copyright: Darko Kolakovic
   Darko Kolakovic 2010-01-15
  */
+#define _WIN32_DCOM //use DCOM to access COM objects
 #include "stdafx.h"
+//#include <comdef.h> //Native C++ compiler COM support
+//#include "wbemcli.h"        //WMI interface declarations (wbemcli.idl)
 #include "KWmi.h" //CWmi class
+#pragma comment(lib, "wbemuuid.lib")
 
 ///////////////////////////////////////////////////////////////////////////////
 // CWmi class implementation
 // To create an application for WMI you must initialize COM, access and set WMI
 // protocols and perform a manual cleanup. Follow next steps:
 //   1. Initialize COM.
-//      Because WMI is based on COM technology, you must perform calls to the 
+//      Because WMI is based on COM technology, you must perform calls to the
 //      CoInitializeEx and CoInitializeSecurity functions to access WMI.
 //   2. Create a connection to a WMI namespace.
-//      By definition, WMI runs in a different process than your application. 
+//      By definition, WMI runs in a different process than your application.
 //      Therefore, you must create a connection between your application and WMI.
 //   3. Set the security levels on the WMI connection.
-//      To use the connection you create to WMI, you must set the impersonation 
+//      To use the connection you create to WMI, you must set the impersonation
 //      and authentication levels for your application.
 //   4. Implement the purpose of your application.
-//      WMI exposes a variety of COM interfaces use to access and manipulate data 
-//      across your enterprise. For more information, see Manipulating Class and 
+//      WMI exposes a variety of COM interfaces use to access and manipulate data
+//      across your enterprise. For more information, see Manipulating Class and
 //      Instance Information, Receiving a WMI Event, and COM API for WMI.
-//      This is where the bulk of your WMI client application should exist, such 
+//      This is where the bulk of your WMI client application should exist, such
 //      as accessing WMI objects or manipulating data.
 //   5. Cleanup and shut down your application.
 //      After you complete your queries to WMI, you should destroy all COM pointers
 //      and shut down your application correctly.
-// Reference: MSDN: Creating a WMI Application Using C++, 
+// Reference: MSDN: Creating a WMI Application Using C++,
 // http://msdn.microsoft.com/en-us/library/aa389762%28VS.85%29.aspx
-
+//
+// Web-Based Enterprise Management (WBEM) is a set of systems management
+// technologies developed to unify the management of distributed computing
+// environments. WBEM is based on Internet standards and DMTF open standards: CIM
+// infrastructure and schema, CIM-XML, CIM operations over HTTP, WS-Management.
 
 //-----------------------------------------------------------------------------
 /*Deafult constructor.
  */
-CWmi::CWmi()
+CWmi::CWmi() :
+  m_hInitializeRes(RPC_E_CHANGED_MODE),
+  m_pIWbemServices(NULL)
 {
 TRACE0("CWmi::CWmi()\n");
 }
@@ -50,20 +60,32 @@ CWmi::~CWmi()
 {
 TRACE0("CWmi::~CWmi()\n");
 Disconnect();
+if(IsCoInitialized())
+  {
+  /*To close the COM library gracefully on a thread, each successful call to
+    CoInitialize or CoInitializeEx, including any call that returns S_FALSE,
+    must be balanced by a corresponding call to CoUninitialize, as the last
+    step in the process.
+   */
+  CoUninitialize();
+  }
 }
 
 //-----------------------------------------------------------------------------
-/*
+/*Initializes necessary resources required to query local or remote WMI objects.
+
+  Returns: true if successful, otherwise returns false.
  */
-bool CWmi::Init(LPCTSTR lpstrDevice //[in] = NULL
-                   )
+bool CWmi::Init(LPCTSTR lpstrDeviceUri //[in] = NULL URI of the correct WMI namespace.
+                //If it is NULL the default local namespace will be used.
+               )
 {
 #ifdef _UNICODE
-  TRACE1(_T("CWmi::Connect(%ws)\n"), lpstrDevice);
+  TRACE1(_T("CWmi::Init(%ws)\n"), lpstrDeviceUri);
 #else
-  TRACE1(_T("CWmi::Connect(%s)\n", lpstrDevice);
+  TRACE1(_T("CWmi::Init(%s)\n", lpstrDeviceUri);
 #endif
-HRESULT hInitializeRes = S_FALSE;
+
 try
   {
   //1. Initialize COM parameters with a call to CoInitializeEx.
@@ -71,10 +93,10 @@ try
     Note: OLE clients require apartment-threading (COINIT_APARTMENTTHREADED).
     Minimum supported client is Windows 2000 Professional.
    */
-  hInitializeRes = CoInitializeEx(NULL, 
+  m_hInitializeRes = CoInitializeEx(NULL,
                                  COINIT_MULTITHREADED
                                  );
-  if(hInitializeRes != RPC_E_CHANGED_MODE)
+  if(IsCoInitialized())
     {
     //2. Initialize COM process security by calling CoInitializeSecurity.
     //   Windows 2000:  Specify the default authentication credentials for
@@ -86,47 +108,75 @@ try
       const DWORD dwAuthnLevel = RPC_C_AUTHN_LEVEL_DEFAULT;
       //additional capabilities of the client or server
       const DWORD dwCapabilities = EOAC_NONE;
-    #endif
-    #ifdef AUTHN_LEVEL_NONE
+    #elif defined (AUTHN_LEVEL_NONE)
       //the authentication level for the process to be used for remote asynchronous
-      //WMI calls to non-Kerberos targets, where the "Local System" account has no 
+      //WMI calls to non-Kerberos targets, where the "Local System" account has no
       //network identity. This will make application less secure, so it is recommeded
       //replacing asynchronous WMI calls with semi-synchronous.
       const DWORD dwAuthnLevel = RPC_C_AUTHN_LEVEL_NONE;
       //additional capabilities of the client or server have to be EOAC_NONE if
       //authentication level is set to RPC_C_AUTHN_LEVEL_NONE
       const DWORD dwCapabilities = EOAC_NONE;
+    #else
+      //the credentials authentication and encryption
+      const DWORD dwAuthnLevel = RPC_C_AUTHN_LEVEL_PKT_PRIVACY;
+      //additional capabilities of the client or server
+      const DWORD dwCapabilities = EOAC_SECURE_REFS;
     #endif
-
-    //the credentials authentication and encryption
-    const DWORD dwAuthnLevel = RPC_C_AUTHN_LEVEL_PKT_PRIVACY;
-    //additional capabilities of the client or server
-    const DWORD dwCapabilities = EOAC_SECURE_REFS;
     HRESULT hResult;
-    hResult = CoInitializeSecurity(NULL,                         //Security descriptor
-                                  -1,                            //COM authentication
-                                  NULL,                          //Authentication services
-                                  NULL,                          //Reserved
-                                  dwAuthnLevel,                  //Default authentication 
-                                  RPC_C_IMP_LEVEL_IMPERSONATE,   //Default Impersonation  
-                                  NULL,                          //Authentication info
-                                  dwCapabilities,                //Additional capabilities 
-                                  NULL);                         //Reserved
+    hResult = CoInitializeSecurity(NULL,              //Security descriptor
+                          -1,                         //COM authentication
+                          NULL,                       //Authentication services
+                          NULL,                       //Reserved
+                          dwAuthnLevel,               //Default authentication
+                          RPC_C_IMP_LEVEL_IMPERSONATE,//Default Impersonation
+                          NULL,                       //Authentication info
+                          dwCapabilities,             //Additional capabilities
+                          NULL);                      //Reserved
     if(SUCCEEDED(hResult))
       {
       //3. Obtain the initial locator to WMI by calling CoCreateInstance.
+      IWbemLocator* pIWbemLocator = NULL; //interface to the WbemLocator service
+      hResult = CoCreateInstance(CLSID_WbemLocator,      //locator class ID
+                                 NULL,                   //not aggregate object
+                                 CLSCTX_INPROC_SERVER,   //indicates a dll running
+                                                         //in same process as caller
+                                 IID_IWbemLocator,       //WBEM interface reference
+                                 (LPVOID *) &pIWbemLocator //instance of
+                                                         //the the WbemLocator
+                                 );
+      if(SUCCEEDED(hResult))
+        {
+        //4. Obtain a pointer to IWbemServices for the root\cimv2 namespace on the local
+        //   or remote computer by calling IWbemLocator::ConnectServer.
+        CComBSTR bstrNamespace = (lpstrDeviceUri != NULL) ? 
+            CComBSTR(lpstrDeviceUri):
+            CComBSTR(NS_WMICIM);
+        if (m_pIWbemServices != NULL)
+            m_pIWbemServices->Release();
 
-      //4. Obtain a pointer to IWbemServices for the root\cimv2 namespace on the local
-      //   computer by calling IWbemLocator::ConnectServer.
+        hResult = pIWbemLocator->ConnectServer(bstrNamespace, //URI of WMI
+                                      NULL,                         //current user name
+                                      NULL,                         //current user password
+                                      0,                            //Locale, current
+                                      NULL,                         //Security flags
+                                      0,                            //Authority
+                                      0,                            //Context object
+                                      &m_pIWbemServices);           //IWbemServices proxy
+        pIWbemLocator->Release();
+        //5. Set IWbemServices proxy security so the WMI service can impersonate the client
+        //   by calling CoSetProxyBlanket.
 
-      //5. Set IWbemServices proxy security so the WMI service can impersonate the client
-      //   by calling CoSetProxyBlanket.
-
-      return true;
+        return true;
+        }
+      else
+        {
+        TRACE1(_T( "  CoCreateInstance() failed (0x%08X).\n"), hResult);
+        }
       }
     else
       {
-      TRACE(_T( "  CoInitializeSecurity() failed.\n"));
+      TRACE1(_T( "  CoInitializeSecurity() failed (0x%08X).\n"), hResult);
       }
     }
 
@@ -136,14 +186,6 @@ catch (...)
   TRACE(_T( "  WMI connection failed.\n"));
   }
 
-if(hInitializeRes != RPC_E_CHANGED_MODE) //Failed after step 1.
-  {
-  /*To close the COM library gracefully on a thread, each successful call to CoInitialize 
-    or CoInitializeEx, including any call that returns S_FALSE, must be balanced by 
-    a corresponding call to CoUninitialize.
-   */
-  CoUninitialize();
-  }
 return false;
 }
 
@@ -153,11 +195,35 @@ return false;
 bool CWmi::Disconnect()
 {
 TRACE(_T("CWmi::Disconnect()\n"));
-if (IsConnected())
+try
   {
+  if (IsConnected())
+    {
+    }
+
+  //m_bConnected = false;
+  if (m_pIWbemServices != NULL)
+    {
+    #ifdef _DEBUG
+      unsigned long nRef = m_pIWbemServices->Release();
+      if (nRef == 0)
+        m_pIWbemServices = NULL;
+      else
+        {
+        TRACE1(_T("  IWbemServices reference counter = %d.\n"), nRef);
+        }
+    #else
+      if (m_pIWbemServices->Release() == 0)
+        m_pIWbemServices = NULL;
+    #endif
+    }
+  return true;
   }
-//m_bConnected = false;
-return true;
+catch(...)
+  {
+  TRACE(_T("  Failed to release resources!\n"));
+  }
+return false;
 }
 
 //-----------------------------------------------------------------------------
@@ -171,6 +237,9 @@ return false;//m_bConnected;
 ///////////////////////////////////////////////////////////////////////////////
 /*****************************************************************************
  * $Log: KWmi.cpp,v $
+ * Revision 1.2  2010/01/27 22:41:27  ddarko
+ * *** empty log message ***
+ *
  * Revision 1.1  2010/01/26 22:47:05  ddarko
  * Initialization of WMI
  *
--- _SrcPool/Cpp/Win/32/KWmi.h
+++ _SrcPool/Cpp/Win/32/KWmi.h
@@ -1,5 +1,5 @@
 /*$RCSfile: KWmi.h,v $: header file
-  $Revision: 1.2 $ $Date: 2010/01/26 22:47:05 $
+  $Revision: 1.3 $ $Date: 2010/01/27 22:41:27 $
   $Author: ddarko $
 
   Microsoft Windows Management Instrumentation (WMI) client.
@@ -16,10 +16,23 @@
   #pragma message ("   #include " __FILE__ )
 #endif
 
+#include <wbemidl.h> //Web-based Enterprise Management (WBEM) interfaces
+
+/*WMI namespace part of the uniform resource identifier (URI).object path of
+  the correct WMI namespace.
+
+  See also: {html: <a href="http://msdn.microsoft.com/en-us/library/aa390342%28VS.85%29.aspx">
+  MSDN: Describing a WMI Namespace Object Path</a> }
+ */
+#define NS_WMICIM     _T("root\\cimv2")
+//Default WMI namespace used to construct uniform resource identifier (URI).object path.
+//Note: Microsoft Windows NT 4.0 and Windows 98 are using this as default space
+//for management
+#define NS_WMIDEFAULT _T("root\\default")
 
 /////////////////////////////////////////////////////////////////////////////
 // CWmi is generic client to the Microsoft Windows Management Instrumentation
-// (WMI) COM service. WMI provides a standardized system management 
+// (WMI) COM service. WMI provides a standardized system management
 // infrastructure on Windows-based operating systems that can be leveraged by
 // a number of different clients.
 // WMI is based on the Common Information Model (CIM) schema class developed
@@ -31,7 +44,11 @@
 //       Windows 2000 requires also  WMI Redistributable Components version 1.0.
 //
 // See also: {html: <a href = "http://msdn.microsoft.com/en-us/library/aa394582%28VS.85%29.aspx">
-// MSDN: Windows Management Instrumentation</a>, 
+// MSDN: Windows Management Instrumentation</a>;<br />
+// <a href="http://technet.microsoft.com/en-us/library/ee692772.aspx">
+// Microsoft TechNet: Windows Management Instrumentation (WMI): Frequently Asked Questions</a>;<br />
+// <a href="http://www.dmtf.org/standards/cim/">
+// DMTF: Common Information Model (CIM) Standards</a> }
 class CWmi
 {
 // Construction
@@ -43,6 +60,8 @@ public:
 public:
 
 private:
+  HRESULT m_hInitializeRes; //result of initialization of the COM library
+  IWbemServices* m_pIWbemServices; //interface to WMI services
 
 // Operations
 public:
@@ -51,19 +70,37 @@ public:
   bool Disconnect();
 
 // Implementation
-public:
-
+private:
+  bool IsCoInitialized();
 // Overrides
 public:
 };
 
-/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // Inlines
 
-/////////////////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+/*Validates if the COM library is initialized properly for use by the calling
+  thread.
+  Returns true if the COM library was initialized successfully on this thread.
+ */
+inline bool CWmi::IsCoInitialized()
+{
+//The COM library was initialized successfully on this thread or
+//the COM library is already initialized on this thread.
+//Note: Error RPC_E_CHANGED_MODE indicates that a previous call to CoInitializeEx
+//set the concurrency model as multithread apartment (MTA), indicating change to
+//single-threaded apartment.
+return ((m_hInitializeRes == S_OK) ||
+        (m_hInitializeRes == S_FALSE) );
+}
+///////////////////////////////////////////////////////////////////////////////
 #endif  //_KWMI_H_
 /*****************************************************************************
  * $Log: KWmi.h,v $
+ * Revision 1.3  2010/01/27 22:41:27  ddarko
+ * *** empty log message ***
+ *
  * Revision 1.2  2010/01/26 22:47:05  ddarko
  * Initialization of WMI
  *
