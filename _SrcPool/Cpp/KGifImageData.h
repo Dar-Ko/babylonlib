f8814cfb35bd5e45327b94cb1ccdce36d4773ea9
ddarko <ddarko>
2011-04-20 21:29:24 +0000

    comment

--- _SrcPool/Cpp/KGifImageData.h
+++ _SrcPool/Cpp/KGifImageData.h
@@ -3,9 +3,9 @@
   $Author: Darko$
 
   Container for GIF(c) image raster data.
-  GIF and 'Graphics Interchange Format' are trademarks of CompuServe, 
+  GIF and 'Graphics Interchange Format' are trademarks of CompuServe,
   Incorporated.
- */ 
+ */
 
 /* Group=Images                                                              */
 
@@ -20,22 +20,26 @@
 #include "KTypedef.h" //uint8 definition
 #include "KGif.h"     //CGifBlock class
 ///////////////////////////////////////////////////////////////////////////////
-/*The image data consists of a sequence of sub-blocks containing an index into 
-  the active color table, for each pixel in the image. Pixel indices are in 
-  order of left to right and from top to bottom. Each index must be within the 
-  range of the size of the active color table, starting at 0. The sequence of 
-  indices is encoded using the LZW Algorithm with variable-length code.
+/*The image data consists of a sequence of sub-blocks containing an index into
+  the active color table, for each pixel in the image. Pixel indices are in
+  order of left to right and from top to bottom. Each index must be within the
+  range of the size of the active color table, starting at 0.
+  The sequence of indices is encoded using the LZW algorithm with variable-length
+  code. The variable-length LZW compression is a variation of the Lempel-Ziv
+  compression algorithm in which variable-length codes are used to replace
+  patterns detected in the original data.
+
   Each data sub-blocks have size at most 255 bytes.
 
                           Bytes
         +------------------+---+
         |   LZW Code Size  | 0 | LZW Minimum Code Size (1 byte)
         +------------------+---+
-        |  Data Sub-block  | i | i [0,255] Image Compressed Data 
+        |  Data Sub-block  | i | i [0,255] Image Compressed Data
         +------------------+---+
         ...
-        +------------------+---+ 
-        |  Data Sub-block  | j | j [0,255] Image Compressed Data 
+        +------------------+---+
+        |  Data Sub-block  | j | j [0,255] Image Compressed Data
         +------------------+---+
         |  LZW Terminator  | n | LZW Compression Terminator (2^LZWMinCode +1)
         +------------------+---+
@@ -67,40 +71,40 @@ public:
   GIF format (c) CompuServe Incorporated, 1987 All rights reserved.
   Graphics Interchange Format (GIF) devised by the UNISYS Corp. and Compuserve.
 
-  While this document describing GIF format is copyrighted, the information 
+  While this document describing GIF format is copyrighted, the information
   contained within is made available for use in computer software without
   royalties, or licensing restrictions.
 
-  The Graphics Interchange Format(c) is the copyright property of 
-  CompuServe Incorporated. Only CompuServe Incorporated is authorized to define, 
-  redefine, enhance, alter, modify or change in any way the definition of 
+  The Graphics Interchange Format(c) is the copyright property of
+  CompuServe Incorporated. Only CompuServe Incorporated is authorized to define,
+  redefine, enhance, alter, modify or change in any way the definition of
   the format.
 
-  CompuServe Incorporated hereby grants a limited, non-exclusive, royalty-free 
+  CompuServe Incorporated hereby grants a limited, non-exclusive, royalty-free
   license for the use of the Graphics Interchange Format(sm) in computer software;
-  computer software utilizing GIF(sm) must acknowledge ownership of the Graphics 
+  computer software utilizing GIF(sm) must acknowledge ownership of the Graphics
   Interchange Format and its Service Mark by CompuServe Incorporated, in User and
-  Technical Documentation. Computer software utilizing GIF, which is distributed 
-  or may be distributed without User or Technical Documentation must display to 
-  the screen or printer a message acknowledging ownership of the Graphics 
-  Interchange Format and the Service Mark by CompuServe Incorporated; 
+  Technical Documentation. Computer software utilizing GIF, which is distributed
+  or may be distributed without User or Technical Documentation must display to
+  the screen or printer a message acknowledging ownership of the Graphics
+  Interchange Format and the Service Mark by CompuServe Incorporated;
   in this case, the acknowledgement may be displayed in an opening screen or
-  leading banner, or a closing screen or trailing banner. A message such as the 
-  following may be used: 
-  "The Graphics Interchange Format(c) is the Copyright property of 
-  CompuServe Incorporated. 
-  GIF(sm) is a Service Mark property of CompuServe Incorporated." 
+  leading banner, or a closing screen or trailing banner. A message such as the
+  following may be used:
+  "The Graphics Interchange Format(c) is the Copyright property of
+  CompuServe Incorporated.
+  GIF(sm) is a Service Mark property of CompuServe Incorporated."
  */
 
 /*****************************************************************************
- * $Log: 
+ * $Log:
  *  3    Biblioteka1.2         2003-09-30 11:09:58  Darko           Replaced DWORD,
  *       WORD with uint32, uint16
- *  2    Biblioteka1.1         2002-08-01 14:58:41  Darko Kolakovic 
- *  1    Biblioteka1.0         2002-08-01 01:23:05  Darko           
+ *  2    Biblioteka1.1         2002-08-01 14:58:41  Darko Kolakovic
+ *  1    Biblioteka1.0         2002-08-01 01:23:05  Darko
  * $
  * 2001 Initial version in Babylon Lib
  * 1990 v.89a enhanced GIF CompuServe Incorporated
- * 1987 v.87a Graphics Interchange Format (GIF) devised by the UNISYS Corp. and 
+ * 1987 v.87a Graphics Interchange Format (GIF) devised by the UNISYS Corp. and
  *  Compuserve
  *****************************************************************************/
