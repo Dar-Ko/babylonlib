aa74b8940aab7c05c3d4a4a39394cd84a6e9a2f1
ddarko <ddarko>
2011-05-05 21:28:36 +0000

    Adjusted test case

--- _SrcPool/Cpp/Samples/Utilities/Src/TestBitset.cpp
+++ _SrcPool/Cpp/Samples/Utilities/Src/TestBitset.cpp
@@ -1,5 +1,5 @@
 /*$RCSfile: TestBitset.cpp,v $: implementation file
-  $Revision: 1.1 $ $Date: 2011/05/02 22:10:58 $
+  $Revision: 1.2 $ $Date: 2011/05/05 21:28:36 $
   $Author: ddarko $
 
   Test bitset handling
@@ -9,12 +9,11 @@
 // Group=Examples
 
 /*Note: MS VC/C++ - Disable precompiled headers (/Yu"StdAfx.h" option)       */
-
-// Each dll/exe must initialize the GUIDs once.  This is done in one of
-// two ways.  If you are not using precompiled headers for the file(s) which
-// initializes the GUIDs, define INITGUID before including <objbase.h>.
-// Microsoft's DEFINE_GUID uses C linkage and therfore generated numbers are global.
-#define INITGUID
+#if (_USE_STL == 20080505) //Test case: StlTools
+  #ifdef _MSC_VER  //MSVC/C++
+    #pragma include_alias("stdafx.h", "stdstl.h")
+  #endif
+#endif
 
 #include "stdafx.h"
 #include "KTypedef.h"
@@ -61,6 +60,9 @@ return bRes;
 //////////////////////////////////////////////////////////////////////////////
 /******************************************************************************
  *$Log: TestBitset.cpp,v $
+ *Revision 1.2  2011/05/05 21:28:36  ddarko
+ *Adjusted test case
+ *
  *Revision 1.1  2011/05/02 22:10:58  ddarko
  *testing <bitset>
  *
