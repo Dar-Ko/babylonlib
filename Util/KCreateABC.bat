f5f3a70dcb6d71f9e7bf7499749296fe322913a1
ddarko <ddarko>
2011-07-08 13:16:27 +0000

    Header

---
+++ Util/KCreateABC.bat
@@ -0,0 +1,44 @@
+@echo off
+REM $RCSfile: KCreateABC.bat,v $: batch command script
+REM $Revision: 1.1 $ $Date: 2011/07/08 13:16:27 $
+REM $Author: ddarko $
+REM
+REM Creates file-system based and alphabetically sorted indexing categories.
+REM
+REM Note: DOS specific
+REM 2005-12-29 Darko Kolakovic
+
+md #
+md A
+md B
+md C
+md D
+md E
+md F
+md G
+md H
+md I
+md J
+md K
+md L
+md M
+md N
+md O
+md P
+md R
+md S
+md T
+md U
+md V
+md W
+md Q
+md X
+md Z
+md Y
+pause
+
+REM ---------------------------------------------------------------------------
+REM $Log: KCreateABC.bat,v $
+REM Revision 1.1  2011/07/08 13:16:27  ddarko
+REM Header
+REM
