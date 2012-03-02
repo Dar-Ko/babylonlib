582a28e50e2f54c38dc2644eaf2270fb28df3c94
ddarko <ddarko>
2012-03-02 22:22:39 +0000

    Linux update

--- _SrcPool/Cpp/KDll.h
+++ _SrcPool/Cpp/KDll.h
@@ -1,5 +1,5 @@
 /*$RCSfile: KDll.h,v $: header file
-  $Revision: 1.8 $ $Date: 2012/02/24 00:09:17 $
+  $Revision: 1.9 $ $Date: 2012/03/02 22:22:39 $
   $Author: ddarko $
 
   Helper class encapsulating a dynamic-link library (DLL) loading.
@@ -21,7 +21,7 @@
     #include <crtdbg.h>
   #endif
 
-  #ifndef TRACE 
+  #ifndef TRACE
     #ifdef _UNICODE
       #define TRACE(msg) _RPTW0(_CRT_WARN, msg)
     #else
@@ -29,7 +29,7 @@
     #endif
   #endif TRACE
 
-  #ifndef TRACE1 
+  #ifndef TRACE1
     #ifdef _UNICODE
       #define TRACE1(msg, arg1) _RPTW1(_CRT_WARN, msg, arg1)
     #else
@@ -37,7 +37,7 @@
     #endif
   #endif TRACE1
 
-  #ifndef TRACE2 
+  #ifndef TRACE2
     #ifdef _UNICODE
       #define TRACE2(msg, arg1, arg2) _RPTW2(_CRT_WARN, msg, arg1, arg2)
     #else
@@ -53,7 +53,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //Helper class encapsulating a Dynamic-Link Library (DLL) loading.
-// 
+//
 class CDll
 {
 public:
@@ -69,7 +69,7 @@ public:
 
 protected:
   HMODULE m_hLibrary; //handle to the module
-  
+
 private:
   CDll(const CDll&);
   CDll& operator=(const CDll&);
@@ -86,8 +86,8 @@ inline CDll::CDll() :
 {
 }
 
-inline CDll::CDll(LPCTSTR szDllName //[in] null-terminated string that names 
-                                    //the DLL file. 
+inline CDll::CDll(LPCTSTR szDllName //[in] null-terminated string that names
+                                    //the DLL file.
                   ) :
    m_hLibrary(NULL)
 {
@@ -124,12 +124,12 @@ return GetSymbolAdr(szSymbolName);
 #if defined (_WIN32) || defined (_WIN16)
 //-----------------------------------------------------------------------------
 /*This function maps the specified DLL file into the address space of the calling
-  process. 
+  process.
 
   With both implicit and explicit linking, Windows first searches for system DLLs,
   such as Kernel32.dll and User32.dll. Windows then searches for the DLLs in
   the following sequence:
-    1. The absolute path, if specified by the DLL file name parameter 
+    1. The absolute path, if specified by the DLL file name parameter
     2. The directory where the executable module for the current process is located.
     3. The current directory.
     4. The Windows system directory.
@@ -137,31 +137,31 @@ return GetSymbolAdr(szSymbolName);
     6. The directories listed in the PATH environment variable.
 
    WinCE specific:
-    1. The absolute path specified by the DLL file name parameter 
-    2. The .exe launch directory 
-    3. The Windows directory 
-    4. ROM DLL files 
-    5. An OEM-specified search path, 
-   
+    1. The absolute path specified by the DLL file name parameter
+    2. The .exe launch directory
+    3. The Windows directory
+    4. ROM DLL files
+    5. An OEM-specified search path,
+
   Returns true if the module is successfuly loaded.
  */
-inline 
-bool CDll::Load(LPCTSTR szDllName //[in] null-terminated string that names 
-                                   //the DLL file. 
-               ) 
+inline
+bool CDll::Load(LPCTSTR szDllName //[in] null-terminated string that names
+                                   //the DLL file.
+               )
 {
  //Reload the library
 Free();
 
 #if definded (_WIN16)
-  //Load a 32-bit DLL within 16-bit code 
+  //Load a 32-bit DLL within 16-bit code
   m_hLibrary = (HMODULE)::LoadLibraryEx32W(szDllName, NULL, 0);
 #elif defined (_WIN32_WCE) //WinCE
   _ASSERT(sizeof(HINSTANCE) == sizeof(HMODULE));
   m_hLibrary = static_cast<HMODULE>(::LoadLibrary(szDllName));
 #else  //WIN32, WIN64
   /*Note Windows Me/98/95:  If you are using LoadLibrary to load a module that
-    contains a resource whose numeric identifier is greater than 0x7FFF, 
+    contains a resource whose numeric identifier is greater than 0x7FFF,
     LoadLibrary fails. If you are attempting to load a 16-bit DLL directly from
     32-bit code, LoadLibrary fails. If you are attempting to load a DLL whose
     subsystem version is greater than 4.0, LoadLibrary fails.
@@ -182,21 +182,21 @@ if (!IsOpen())
     /*BUG: LoadLibrary and LoadLibraryEx Functions Fail If Directory Name Contains
            a Period.
       Windows specific
-      
+
       The LoadLibrary or the LoadLibraryEx function may fail with error 126
       (ERROR_MOD_NOT_FOUND) if a directory name in the path contains a period.
-      According to the Microsoft Platform Software Development Kit (SDK) 
+      According to the Microsoft Platform Software Development Kit (SDK)
       documentation for LoadLibrary and LoadLibraryEx, if you do not include the
       file name extension with the file name, the system tries to load a file with
-      no extension. If such a file does not exist, the .dll extension is appended 
+      no extension. If such a file does not exist, the .dll extension is appended
       to the file name, and the system tries to load the file again.
-      The error occurs only if the directory in the path that is passed to 
-      LoadLibrary or LoadLibraryEx contains a period (.) and if no extension is 
+      The error occurs only if the directory in the path that is passed to
+      LoadLibrary or LoadLibraryEx contains a period (.) and if no extension is
       appended to the .dll file name.
       To work around this problem, specify a fully qualified file name that includes
       the file name extension:
           hDll = LoadLibrary("C:\\MY.DLLs\\ABC.DLL");
-      instead 
+      instead
           hDLL = LoadLibrary("C:\\MY.DLLs\\ABC"); //Failed with ERROR_MOD_NOT_FOUND
        Ref:. MSDN Article ID: 324468
      */
@@ -230,7 +230,7 @@ return (m_hLibrary > NULL); //>= (HINSTANCE)32
   no compile-time type checking, make sure that the parameters to the function
   are correct so that you do not overstep the memory allocated on the stack and
   cause an access violation.
-  
+
   Returns the address of the exported function or variable, if the function succeeds.
   If the function fails, the return value is NULL. To get extended error information,
   call GetLastError().
@@ -246,9 +246,9 @@ return (void*)::GetProcAddress(m_hLibrary, szSymbolName);
 }
 
 //-----------------------------------------------------------------------------
-/*This function decrements the reference count of the loaded DLL module. 
+/*This function decrements the reference count of the loaded DLL module.
   When the reference count reaches zero, the module is unmapped from the address
-  space of the calling process. 
+  space of the calling process.
  */
 inline bool CDll::Free()
 {
@@ -261,7 +261,7 @@ if (IsOpen())
       TRACE2(TEXT("  FreeLibrary(%08X) failed 0x%08x!\n"), m_hLibrary, dwError);
     #endif
     //TODO GetLastError()
-    return false;  
+    return false;
     }
    m_hLibrary = NULL; //FixMe: Even if it is handle < 32
   }
@@ -276,25 +276,34 @@ return true;
   #ifndef HMODULE
     #define HMODULE void*
   #endif
-  
+
 //-----------------------------------------------------------------------------
 /*This function maps the specified DLL file into the address space of the calling
-  process. 
+  process.
 
+  If the environment variable LD_LIBRARY_PATH was defined to contain
+  a colon-separated list of directories, then these are searched.
+  This variable is ignored for set-user-ID and set-group-ID programs.
+
+  If szDllName is a NULL pointer, then the returned handle is for the main
+  program. This handle resolves symbols in the main program, then sinbols in all 
+  shared libraries loaded at program startup and then in all shared libraries loaded 
+  with the flag RTLD_GLOBAL.
+ 
   Returns true if the module is successfuly loaded.
  */
-inline 
-bool CDll::Load(LPCTSTR szDllName //[in] null-terminated string that names 
-                                   //the shared library file. 
-               ) 
+inline
+bool CDll::Load(LPCTSTR szDllName //[in] null-terminated string that names
+                                   //the shared library file.
+               )
 {
  //Reload the library
 Free();
 #ifdef _UNICODE
  #error "Convert shared library name to single byte character string"
 #endif
-/*Perform lazy binding. Only resolve symbols as the code that references 
-  them is executed. If the symbol is never referenced, then it is never 
+/*Perform lazy binding. Only resolve symbols as the code that references
+  them is executed. If the symbol is never referenced, then it is never
   resolved. Lazy binding is only performed for function references; references
   to variables are always immediately bound when the library is loaded.
  */
@@ -327,7 +336,7 @@ return (m_hLibrary > NULL);
   no compile-time type checking, make sure that the parameters to the function
   are correct so that you do not overstep the memory allocated on the stack and
   cause an access violation.
-  
+
    Returns the address of the exported function or variable, if the function succeeds.
    If the function fails, the return value is NULL. To get extended error information,
    call dlerror().
@@ -341,34 +350,34 @@ return dlsym(m_hLibrary, szSymbolName);
 }
 
 //-----------------------------------------------------------------------------
-/*This function decrements the reference count of the loaded DLL module. 
-  When the reference count reaches zero, then the dynamic library is unloaded. 
+/*This function decrements the reference count of the loaded DLL module.
+  When the reference count reaches zero, then the dynamic library is unloaded.
  */
 inline bool CDll::Free()
 {
 if (IsOpen())
   {
-  if (dlclose(m_hLibrary) == FALSE)
+  if (dlclose(m_hLibrary) != 0)
     {
     #ifdef _DEBUG
       char* szError = dlerror();
-	  if(szError == NULL)
-	    szError = "";
+    if(szError == NULL)
+      szError = "";
       TRACE2(TEXT("CDll::Free(%08X) failed: %s!\n"), m_hLibrary, szError);
     #endif
-    return false;  
+    return false;
     }
    m_hLibrary = NULL;
   }
 return true;
-}  
+}
 #endif //LINUX
 ///////////////////////////////////////////////////////////////////////////////
 #endif  //_KDLL_H_
 /******************************************************************************
  *$Log: KDll.h,v $
- *Revision 1.8  2012/02/24 00:09:17  ddarko
- *_WIN32_WCE
+ *Revision 1.9  2012/03/02 22:22:39  ddarko
+ *Linux update
  *
  *Revision 1.7  2012/02/23 18:39:28  ddarko
  *Comment
